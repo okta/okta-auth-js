12f543189674d2c8e7b2c0c9565cbff602b96836
lboyette-okta <lboyette@okta.com>
2016-05-25 11:32:57 -0700

    Change require.js to webpack for tests (#16)
    bacon: test
    resolves: OKTA-90172

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,28 +7,14 @@ module.exports = function(grunt) {
       phantom: {
         options: {
           specs: [
-            'test/util/jasmine-extensions.js', // Included here for lodash
-            'test/spec/*.js'
+            'dist/test/tests.js'
           ],
-          template: require('grunt-template-jasmine-requirejs'),
-          templateOptions: {
-            requireConfigFile: 'test/main.js'
-          },
           outfile: 'test/SpecRunner.html',
           junit: {
             path: 'build2/reports/jasmine'
           },
           keepRunner: true,
-          summary: true,
-          host: 'http://localhost:9696'
-        }
-      }
-    },
-
-    connect: {
-      server: {
-        options: {
-          port: 9696
+          summary: true
         }
       }
     },
@@ -37,9 +23,14 @@ module.exports = function(grunt) {
       lint: {
         command: 'npm run lint'
       },
+      test: {
+        command: 'npm run test'
+      },
+      // Builds an AMD version that requires jQuery and Q
       AMDJqueryQ: {
         command: 'npm run build:AMDJqueryQ'
       },
+      // Builds a UMD version that has no dependencies
       UMDNoDependencies: {
         command: 'npm run build'
       }
@@ -47,15 +38,8 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-jasmine');
-  grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-shell');
 
-  grunt.registerTask('test', ['shell:lint', 'buildAMDWithoutJqueryOrQ', 'connect:server', 'jasmine:phantom']);
-  grunt.registerTask('default', ['test', 'buildUMDWithNoDependencies']);
-
-  // Builds an AMD version that requires jQuery and Q
-  grunt.registerTask('buildAMDWithoutJqueryOrQ', ['shell:AMDJqueryQ']);
-
-  // Builds a UMD version that has no dependencies
-  grunt.registerTask('buildUMDWithNoDependencies', ['shell:UMDNoDependencies']);
+  grunt.registerTask('test', ['shell:lint', 'shell:AMDJqueryQ', 'jasmine:phantom']);
+  grunt.registerTask('default', ['shell:UMDNoDependencies', 'shell:AMDJqueryQ']);
 };
