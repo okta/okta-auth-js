5ba3aae36188b19dfd89c22b1b3e9d223813d545
lboyette-okta <lboyette@okta.com>
2016-07-12 15:49:35 -0700

    Changed setup to be sync

--- test/spec/token.js
+++ test/spec/token.js
@@ -1,40 +1,29 @@
 define(function(require) {
   var OktaAuth = require('OktaAuth');
-  var Q = require('q');
   var tokens = require('../util/tokens');
 
   describe('token.decode', function () {
 
-    function setup() {
-      return Q.resolve(new OktaAuth({
-        url: 'http://example.okta.com'
-      }));
+    function setupSync() {
+      return new OktaAuth({ url: 'http://example.okta.com' });
     }
 
-    it('correctly decodes a token', function (done) {
-      return setup()
-      .then(function (oa) {
-        var decodedToken = oa.token.decode(tokens.unicodeToken);
-        expect(decodedToken).toDeepEqual(tokens.unicodeDecoded);
-        done();
-      });
+    it('correctly decodes a token', function () {
+      var oa = setupSync();
+      var decodedToken = oa.token.decode(tokens.unicodeToken);
+      expect(decodedToken).toDeepEqual(tokens.unicodeDecoded);
     });
 
-    it('throws an error for a malformed token', function (done) {
-      return setup()
-      .then(function (oa) {
-        return oa.token.decode('malformedToken');
-      })
-      .then(function (res) {
+    it('throws an error for a malformed token', function () {
+      var oa = setupSync();
+      try {
+        var res = oa.token.decode('malformedToken');
         // Should never hit this
         expect(res).toBeNull();
-        done();
-      })
-      .fail(function (err) {
-        expect(err.name).toEqual('AuthSdkError');
-        expect(err.errorSummary).toBeDefined();
-        done();
-      });
+      } catch (e) {
+        expect(e.name).toEqual('AuthSdkError');
+        expect(e.errorSummary).toBeDefined();
+      }
     });
   });
 });
