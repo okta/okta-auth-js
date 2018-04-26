f872b346a13badfad795cab104a2716f17938977
alexeystadnikov-okta <alexey.stadnikov@okta.com>
2018-04-26 19:52:21 +0300

    :seedling: Add support to use rememberDevice param in polling and verify. (#111)
    Similar to autoPush, we now support rememberDevice function that will be evaluated before making the request.
    
    Resolves: OKTA-158993

--- test/spec/mfa-challenge.js
+++ test/spec/mfa-challenge.js
@@ -50,7 +50,7 @@ define(function(require) {
         setup: {
           status: 'mfa-challenge-sms',
           request: {
-            uri: '/api/v1/authn/factors/smsigwDlH85L9FyQK0g3/verify',
+            uri: '/api/v1/authn/factors/smsigwDlH85L9FyQK0g3/verify?rememberDevice=false',
             data: {
               stateToken: '00rt1IY9c6Q3RVc4a2jJPbS2uAtFNWJz_d8A26KTdF',
               passCode: '123456'
@@ -239,6 +239,94 @@ define(function(require) {
       });
 
       util.itMakesCorrectRequestResponse({
+        title: 'allows verification with rememberDevice as a function',
+        setup: {
+          status: 'mfa-challenge-sms',
+          request: {
+            uri: '/api/v1/authn/factors/smsigwDlH85L9FyQK0g3/verify?rememberDevice=true',
+            data: {
+              stateToken: '00rt1IY9c6Q3RVc4a2jJPbS2uAtFNWJz_d8A26KTdF',
+              passCode: '123456'
+            }
+          },
+          response: 'success'
+        },
+        execute: function (test) {
+          return test.trans.verify({
+            passCode: '123456',
+            rememberDevice: function () {
+              return true;
+            }
+          });
+        }
+      });
+
+      util.itMakesCorrectRequestResponse({
+        title: 'allows verification when rememberDevice function returns truthy value',
+        setup: {
+          status: 'mfa-challenge-sms',
+          request: {
+            uri: '/api/v1/authn/factors/smsigwDlH85L9FyQK0g3/verify?rememberDevice=true',
+            data: {
+              stateToken: '00rt1IY9c6Q3RVc4a2jJPbS2uAtFNWJz_d8A26KTdF',
+              passCode: '123456'
+            }
+          },
+          response: 'success'
+        },
+        execute: function (test) {
+          return test.trans.verify({
+            passCode: '123456',
+            rememberDevice: function () {
+              return 'test';
+            }
+          });
+        }
+      });
+
+      util.itMakesCorrectRequestResponse({
+        title: 'allows verification when rememberDevice function returns falsy value',
+        setup: {
+          status: 'mfa-challenge-sms',
+          request: {
+            uri: '/api/v1/authn/factors/smsigwDlH85L9FyQK0g3/verify?rememberDevice=false',
+            data: {
+              stateToken: '00rt1IY9c6Q3RVc4a2jJPbS2uAtFNWJz_d8A26KTdF',
+              passCode: '123456'
+            }
+          },
+          response: 'success'
+        },
+        execute: function (test) {
+          return test.trans.verify({
+            passCode: '123456',
+            rememberDevice: function () {
+              return '';
+            }
+          });
+        }
+      });
+
+      util.itErrorsCorrectly({
+        title: 'throws an error when rememberDevice function throws an error',
+        setup: {
+          status: 'mfa-challenge-sms'
+        },
+        execute: function (test) {
+          return test.trans.verify({
+            passCode: '123456',
+            rememberDevice: function () {
+              throw new Error('test');
+            }
+          });
+        },
+        expectations: function (test, err) {
+          expect(err.name).toEqual('AuthSdkError');
+          expect(err.errorSummary).toEqual('RememberDevice resulted in an error.');
+        }
+      });
+
+      util.itMakesCorrectRequestResponse({
         title: 'allows verification with autoPush and rememberDevice true',
         setup: {
           status: 'mfa-challenge-sms',
@@ -265,7 +353,7 @@ define(function(require) {
         setup: {
           status: 'mfa-challenge-sms',
           request: {
-            uri: '/api/v1/authn/factors/smsigwDlH85L9FyQK0g3/verify?autoPush=false',
+            uri: '/api/v1/authn/factors/smsigwDlH85L9FyQK0g3/verify?autoPush=false&rememberDevice=false',
             data: {
               stateToken: '00rt1IY9c6Q3RVc4a2jJPbS2uAtFNWJz_d8A26KTdF',
               passCode: '123456'
@@ -368,7 +456,7 @@ define(function(require) {
       });
 
       util.itMakesCorrectRequestResponse({
-        title: 'allows polling for push with rememberDevice',
+        title: 'allows polling for push with rememberDevice true',
         setup: {
           status: 'mfa-challenge-push',
           calls: [
@@ -405,6 +493,48 @@ define(function(require) {
           return test.trans.poll({
             delay: 0,
             rememberDevice: true
+          });
+        }
+      });
+
+      util.itMakesCorrectRequestResponse({
+        title: 'allows polling for push with rememberDevice false',
+        setup: {
+          status: 'mfa-challenge-push',
+          calls: [
+            {
+              request: {
+                uri: '/api/v1/authn/factors/opf492vmb3s1blLTs0h7/verify?rememberDevice=false',
+                data: {
+                  stateToken: '00T4jcVNRzJy5dkWJ4P7c9051dY3FUYY9O2zvbU_vI'
+                }
+              },
+              response: 'mfa-challenge-push'
+            },
+            {
+              request: {
+                uri: '/api/v1/authn/factors/opf492vmb3s1blLTs0h7/verify?rememberDevice=false',
+                data: {
+                  stateToken: '00T4jcVNRzJy5dkWJ4P7c9051dY3FUYY9O2zvbU_vI'
+                }
+              },
+              response: 'mfa-challenge-push'
+            },
+            {
+              request: {
+                uri: '/api/v1/authn/factors/opf492vmb3s1blLTs0h7/verify?rememberDevice=false',
+                data: {
+                  stateToken: '00T4jcVNRzJy5dkWJ4P7c9051dY3FUYY9O2zvbU_vI'
+                }
+              },
+              response: 'success'
+            }
+          ]
+        },
+        execute: function (test) {
+          return test.trans.poll({
+            delay: 0,
+            rememberDevice: false
           });
         }
       });
@@ -694,6 +824,206 @@ define(function(require) {
       });
 
       util.itMakesCorrectRequestResponse({
+        title: 'allows polling for push with rememberDevice as a function',
+        setup: {
+          status: 'mfa-challenge-push',
+          calls: [
+            {
+              request: {
+                uri: '/api/v1/authn/factors/opf492vmb3s1blLTs0h7/verify?rememberDevice=true',
+                data: {
+                  stateToken: '00T4jcVNRzJy5dkWJ4P7c9051dY3FUYY9O2zvbU_vI'
+                }
+              },
+              response: 'mfa-challenge-push'
+            },
+            {
+              request: {
+                uri: '/api/v1/authn/factors/opf492vmb3s1blLTs0h7/verify?rememberDevice=true',
+                data: {
+                  stateToken: '00T4jcVNRzJy5dkWJ4P7c9051dY3FUYY9O2zvbU_vI'
+                }
+              },
+              response: 'mfa-challenge-push'
+            },
+            {
+              request: {
+                uri: '/api/v1/authn/factors/opf492vmb3s1blLTs0h7/verify?rememberDevice=true',
+                data: {
+                  stateToken: '00T4jcVNRzJy5dkWJ4P7c9051dY3FUYY9O2zvbU_vI'
+                }
+              },
+              response: 'success'
+            }
+          ]
+        },
+        execute: function (test) {
+          return test.trans.poll({
+            delay: 0,
+            rememberDevice: function () {
+              return true;
+            }
+          });
+        }
+      });
+
+      util.itMakesCorrectRequestResponse({
+        title: 'allows polling for push with rememberDevice value changing during poll',
+        setup: {
+          status: 'mfa-challenge-push',
+          calls: [
+            {
+              request: {
+                uri: '/api/v1/authn/factors/opf492vmb3s1blLTs0h7/verify?rememberDevice=true',
+                data: {
+                  stateToken: '00T4jcVNRzJy5dkWJ4P7c9051dY3FUYY9O2zvbU_vI'
+                }
+              },
+              response: 'mfa-challenge-push'
+            },
+            {
+              request: {
+                uri: '/api/v1/authn/factors/opf492vmb3s1blLTs0h7/verify?rememberDevice=true',
+                data: {
+                  stateToken: '00T4jcVNRzJy5dkWJ4P7c9051dY3FUYY9O2zvbU_vI'
+                }
+              },
+              response: 'mfa-challenge-push'
+            },
+            {
+              request: {
+                uri: '/api/v1/authn/factors/opf492vmb3s1blLTs0h7/verify?rememberDevice=false',
+                data: {
+                  stateToken: '00T4jcVNRzJy5dkWJ4P7c9051dY3FUYY9O2zvbU_vI'
+                }
+              },
+              response: 'success'
+            }
+          ]
+        },
+        execute: function (test) {
+          var count = 1;
+          return test.trans.poll({
+            delay: 0,
+            rememberDevice: function () {
+              if(count === 3) {
+                return false;
+              }
+              count ++;
+              return true;
+            }
+          });
+        }
+      });
+
+      util.itMakesCorrectRequestResponse({
+        title: 'allows polling for push when rememberDevice function returns truthy value',
+        setup: {
+          status: 'mfa-challenge-push',
+          calls: [
+            {
+              request: {
+                uri: '/api/v1/authn/factors/opf492vmb3s1blLTs0h7/verify?rememberDevice=true',
+                data: {
+                  stateToken: '00T4jcVNRzJy5dkWJ4P7c9051dY3FUYY9O2zvbU_vI'
+                }
+              },
+              response: 'mfa-challenge-push'
+            },
+            {
+              request: {
+                uri: '/api/v1/authn/factors/opf492vmb3s1blLTs0h7/verify?rememberDevice=true',
+                data: {
+                  stateToken: '00T4jcVNRzJy5dkWJ4P7c9051dY3FUYY9O2zvbU_vI'
+                }
+              },
+              response: 'mfa-challenge-push'
+            },
+            {
+              request: {
+                uri: '/api/v1/authn/factors/opf492vmb3s1blLTs0h7/verify?rememberDevice=true',
+                data: {
+                  stateToken: '00T4jcVNRzJy5dkWJ4P7c9051dY3FUYY9O2zvbU_vI'
+                }
+              },
+              response: 'success'
+            }
+          ]
+        },
+        execute: function (test) {
+          return test.trans.poll({
+            delay: 0,
+            rememberDevice: function () {
+              return 'test';
+            }
+          });
+        }
+      });
+
+      util.itMakesCorrectRequestResponse({
+        title: 'allows polling for push when rememberDevice function returns falsy value',
+        setup: {
+          status: 'mfa-challenge-push',
+          calls: [
+            {
+              request: {
+                uri: '/api/v1/authn/factors/opf492vmb3s1blLTs0h7/verify?rememberDevice=false',
+                data: {
+                  stateToken: '00T4jcVNRzJy5dkWJ4P7c9051dY3FUYY9O2zvbU_vI'
+                }
+              },
+              response: 'mfa-challenge-push'
+            },
+            {
+              request: {
+                uri: '/api/v1/authn/factors/opf492vmb3s1blLTs0h7/verify?rememberDevice=false',
+                data: {
+                  stateToken: '00T4jcVNRzJy5dkWJ4P7c9051dY3FUYY9O2zvbU_vI'
+                }
+              },
+              response: 'mfa-challenge-push'
+            },
+            {
+              request: {
+                uri: '/api/v1/authn/factors/opf492vmb3s1blLTs0h7/verify?rememberDevice=false',
+                data: {
+                  stateToken: '00T4jcVNRzJy5dkWJ4P7c9051dY3FUYY9O2zvbU_vI'
+                }
+              },
+              response: 'success'
+            }
+          ]
+        },
+        execute: function (test) {
+          return test.trans.poll({
+            delay: 0,
+            rememberDevice: function () {
+              return '';
+            }
+          });
+        }
+      });
+
+      util.itErrorsCorrectly({
+        title: 'throws an error when rememberDevice function throws an error during polling',
+        setup: {
+          status: 'mfa-challenge-push'
+        },
+        execute: function (test) {
+          return test.trans.poll({
+            delay: 0,
+            rememberDevice: function () {
+              throw new Error('test');
+            }
+          });
+        },
+        expectations: function (test, err) {
+          expect(err.name).toEqual('AuthSdkError');
+          expect(err.errorSummary).toEqual('RememberDevice resulted in an error.');
+        }
+      });
+
+      util.itMakesCorrectRequestResponse({
         title: 'does not include autoPush for polling for push if autoPush undefined',
         setup: {
           status: 'mfa-challenge-push',
@@ -821,13 +1151,13 @@ define(function(require) {
       });
 
       util.itMakesCorrectRequestResponse({
-        title: 'does not include rememberDevice for polling for push if rememberDevice is falsy',
+        title: 'allows polling for push if rememberDevice is falsy',
         setup: {
           status: 'mfa-challenge-push',
           calls: [
             {
               request: {
-                uri: '/api/v1/authn/factors/opf492vmb3s1blLTs0h7/verify?autoPush=true',
+                uri: '/api/v1/authn/factors/opf492vmb3s1blLTs0h7/verify?autoPush=true&rememberDevice=false',
                 data: {
                   stateToken: '00T4jcVNRzJy5dkWJ4P7c9051dY3FUYY9O2zvbU_vI'
                 }
@@ -836,7 +1166,7 @@ define(function(require) {
             },
             {
               request: {
-                uri: '/api/v1/authn/factors/opf492vmb3s1blLTs0h7/verify?autoPush=true',
+                uri: '/api/v1/authn/factors/opf492vmb3s1blLTs0h7/verify?autoPush=true&rememberDevice=false',
                 data: {
                   stateToken: '00T4jcVNRzJy5dkWJ4P7c9051dY3FUYY9O2zvbU_vI'
                 }
@@ -845,7 +1175,7 @@ define(function(require) {
             },
             {
               request: {
-                uri: '/api/v1/authn/factors/opf492vmb3s1blLTs0h7/verify?autoPush=true',
+                uri: '/api/v1/authn/factors/opf492vmb3s1blLTs0h7/verify?autoPush=true&rememberDevice=false',
                 data: {
                   stateToken: '00T4jcVNRzJy5dkWJ4P7c9051dY3FUYY9O2zvbU_vI'
                 }
