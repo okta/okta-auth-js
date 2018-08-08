af429f45abca88a5350728f847799d3ef99f7c89
Jordan Melberg <jordan.melberg@okta.com>
2018-08-08 11:31:41 -0700

    Update scripts to account for yarn/node upgrades (#148)

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
-source $OKTA_HOME/$REPO/scripts/setup.sh
+source ${OKTA_HOME}/${REPO}/scripts/setup.sh
 
 REGISTRY="${ARTIFACTORY_URL}/api/npm/npm-okta"
 
 export TEST_SUITE_TYPE="build"
 
-if [ -n "$action_branch" ];
+if [ -n "${action_branch}" ];
 then
-  echo "Publishing from bacon task using branch $action_branch"
-  TARGET_BRANCH=$action_branch
+  echo "Publishing from bacon task using branch ${action_branch}"
+  TARGET_BRANCH=${action_branch}
 else
-  echo "Publishing from bacon testSuite using branch $BRANCH"
-  TARGET_BRANCH=$BRANCH
+  echo "Publishing from bacon testSuite using branch ${BRANCH}"
+  TARGET_BRANCH=${BRANCH}
 fi
 
 if ! ci-update-package --branch ${TARGET_BRANCH}; then
   echo "ci-update-package failed! Exiting..."
-  exit $FAILED_SETUP
+  exit ${FAILED_SETUP}
 fi
 
 if ! npm publish --registry ${REGISTRY}; then
   echo "npm publish failed! Exiting..."
-  exit $PUBLISH_ARTIFACTORY_FAILURE
+  exit ${PUBLISH_ARTIFACTORY_FAILURE}
 fi
 
 DATALOAD=$(ci-pkginfo -t dataload)
 if ! artifactory_curl -X PUT -u ${ARTIFACTORY_CREDS} ${DATALOAD} -v -f; then
   echo "artifactory_curl failed! Exiting..."
-  exit $PUBLISH_ARTIFACTORY_FAILURE
+  exit ${PUBLISH_ARTIFACTORY_FAILURE}
 fi
 
-exit $SUCCESS
+exit ${SUCCESS}
