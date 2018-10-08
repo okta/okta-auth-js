0d5a5bac17582dbe5c69c66178cf0fd4b0304ade
Jordan Melberg <jordan.melberg@okta.com>
2018-10-08 11:05:27 -0700

    Update dependencies (#162)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,6 @@
 /* globals module */
 module.exports = function(grunt) {
-
   grunt.initConfig({
-
     jasmine: {
       phantom: {
         options: {
@@ -18,26 +16,6 @@ module.exports = function(grunt) {
         }
       }
     },
-
-    shell: {
-      lint: {
-        command: 'npm run lint'
-      },
-      test: {
-        command: 'npm run test'
-      },
-      buildTests: {
-        command: 'npm run build:tests'
-      },
-      // Builds a UMD version that has no dependencies
-      UMDNoDependencies: {
-        command: 'npm run build'
-      }
-    }
   });
-
   grunt.loadNpmTasks('grunt-contrib-jasmine');
-  grunt.loadNpmTasks('grunt-shell');
-
-  grunt.registerTask('test', ['shell:lint', 'shell:buildTests', 'jasmine:phantom']);
 };
