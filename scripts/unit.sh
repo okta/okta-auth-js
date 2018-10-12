6c5b8f7cc5a5961610ccc801bd67552aead10f36
jmelberg <jordan.melberg@okta.com>
2018-10-12 12:31:11 -0700

    Updated round II

--- scripts/unit.sh
+++ scripts/unit.sh
@@ -3,9 +3,9 @@
 source ${OKTA_HOME}/${REPO}/scripts/setup.sh
 
 export TEST_SUITE_TYPE="jsunit"
-export TEST_RESULT_FILE_DIR="${REPO}/build2/reports/"
+export TEST_RESULT_FILE_DIR="${REPO}/build2/reports/unit"
 
-if ! npm run test:unit; then
+if ! npm run test:report; then
   echo "unit failed! Exiting..."
   exit ${TEST_FAILURE}
 fi
