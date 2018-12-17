459fe57127053491221e4a1c358e4f56417e061c
Ali Gasymov <alik0211alik@gmail.com>
2018-12-17 20:13:58 +0400

    :zap: Use hardtack instead of js-cookie

--- test/spec/cookies.js
+++ test/spec/cookies.js
@@ -1,32 +1,32 @@
 var Cookies  = require('../../lib/cookies');
-var JsCookie = require('js-cookie');
+var hardtack = require('hardtack');
 
 describe('cookie', function () {
   beforeEach(function () {
-    jest.spyOn(JsCookie, 'get');
-    jest.spyOn(JsCookie, 'set');
-    jest.spyOn(JsCookie, 'remove');
+    jest.spyOn(hardtack, 'get');
+    jest.spyOn(hardtack, 'set');
+    jest.spyOn(hardtack, 'remove');
   });
 
   describe('setCookie',  function ()  {
     it('proxies JsCookie.set',  function ()  {
       Cookies.setCookie('foo', 'bar');
-      expect(JsCookie.set).toHaveBeenCalledWith('foo', 'bar', {
+      expect(hardtack.set).toHaveBeenCalledWith('foo', 'bar', {
         path: '/'
       });
     });
 
     it('proxies JsCookie.set with an expiry time',  function ()  {
       Cookies.setCookie('foo', 'bar', '2038-01-19T03:14:07.000Z');
-      expect(JsCookie.set).toHaveBeenCalledWith('foo', 'bar', {
+      expect(hardtack.set).toHaveBeenCalledWith('foo', 'bar', {
         path: '/',
-        expires: new Date('2038-01-19T03:14:07.000Z')
+        expires: new Date('2038-01-19T03:14:07.000Z').toUTCString()
       });
     });
 
     it('proxies JsCookie.set with an invalid expiry time',  function ()  {
       Cookies.setCookie('foo', 'bar', 'not a valid date');
-      expect(JsCookie.set).toHaveBeenCalledWith('foo', 'bar', {
+      expect(hardtack.set).toHaveBeenCalledWith('foo', 'bar', {
         path: '/'
       });
     });
@@ -35,14 +35,17 @@ describe('cookie', function () {
   describe('getCookie',  function ()  {
     it('proxies Cookie.getCookie',  function ()  {
       Cookies.getCookie('foo');
-      expect(JsCookie.get).toHaveBeenCalledWith('foo');
+      expect(hardtack.get).toHaveBeenCalledWith('foo');
     });
   });
 
   describe('deleteCookie',  function ()  {
     it('proxies JsCookie.remove',  function ()  {
       Cookies.deleteCookie('foo');
-      expect(JsCookie.remove).toHaveBeenCalledWith('foo', { path: '/' });
+      expect(hardtack.remove).toHaveBeenCalledWith('foo', {
+        expires: "Thu, 01 Jan 1970 00:00:01 GMT",
+        path: '/'
+      });
     });
   });
 });
