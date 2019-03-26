6d9743966f28a07fda16ecd8bb9856e01cd0b9bf
Nikhil Venkatraman <nikhil.venkatraman@gmail.com>
2019-03-26 15:45:56 -0700

    Update publish script to latest

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,14 +2,25 @@
 
 cd ${OKTA_HOME}/${REPO}
 
-# Use newer, faster npm
-npm install -g npm@4.0.2
+# Yarn does not utilize the npmrc/yarnrc registry configuration
+# if a lockfile is present. This results in `yarn install` problems
+# for private registries. Until yarn@2.0.0 is released, this is our current
+# workaround.
+#
+# Related issues:
+#  - https://github.com/yarnpkg/yarn/issues/5892
+#  - https://github.com/yarnpkg/yarn/issues/3330
 
-# Install required dependencies
-npm install -g @okta/ci-update-package
-npm install -g @okta/ci-pkginfo
+YARN_REGISTRY=https://registry.yarnpkg.com
+OKTA_REGISTRY=${ARTIFACTORY_URL}/api/npm/npm-okta-master
 
-if ! npm install --no-optional --unsafe-perm; then
-  echo "npm install failed! Exiting..."
+# Replace yarn artifactory with Okta's
+sed -i "s#${YARN_REGISTRY}#${OKTA_REGISTRY}#g" yarn.lock
+
+if ! yarn install; then
+  echo "yarn install failed! Exiting..."
   exit ${FAILED_SETUP}
 fi
+
+# Revert the origional change
+# sed -i "s#${OKTA_REGISTRY}#${YARN_REGISTRY}#" yarn.lock
