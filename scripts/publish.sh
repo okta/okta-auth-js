770b1e9e68df49d015574e4c7e032a6145ef8fb4
nikhilvenkatraman-okta <nikhil.venkatraman@okta.com>
2019-03-26 15:56:40 -0700

    Fix publish script (#198)
    - Update publish script to latest

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
 
-source $OKTA_HOME/$REPO/scripts/setup.sh
+source ${OKTA_HOME}/${REPO}/scripts/setup.sh
 
 REGISTRY="${ARTIFACTORY_URL}/api/npm/npm-okta"
 
 export TEST_SUITE_TYPE="build"
 
-if [ -n "$action_branch" ];
+# Install required dependencies
+export PATH="${PATH}:$(yarn global bin)"
+yarn global add @okta/ci-update-package
+yarn global add @okta/ci-pkginfo
+
+if [ -n "${action_branch}" ];
 then
-  echo "Publishing from bacon task using branch $action_branch"
-  TARGET_BRANCH=$action_branch
+  echo "Publishing from bacon task using branch ${action_branch}"
+  TARGET_BRANCH=${action_branch}
 else
-  echo "Publishing from bacon testSuite using branch $BRANCH"
-  TARGET_BRANCH=$BRANCH
+  echo "Publishing from bacon testSuite using branch ${BRANCH}"
+  TARGET_BRANCH=${BRANCH}
 fi
 
 if ! ci-update-package --branch ${TARGET_BRANCH}; then
   echo "ci-update-package failed! Exiting..."
-  exit $FAILED_SETUP
+  exit ${FAILED_SETUP}
 fi
 
+### looks like ci-update-package is not compatible with `yarn publish`
+### which expects new-version is passed via command line parameter.
+### keep using npm for now
 if ! npm publish --registry ${REGISTRY}; then
   echo "npm publish failed! Exiting..."
-  exit $PUBLISH_ARTIFACTORY_FAILURE
+  exit ${PUBLISH_ARTIFACTORY_FAILURE}
 fi
 
 DATALOAD=$(ci-pkginfo -t dataload)
 if ! artifactory_curl -X PUT -u ${ARTIFACTORY_CREDS} ${DATALOAD} -v -f; then
   echo "artifactory_curl failed! Exiting..."
-  exit $PUBLISH_ARTIFACTORY_FAILURE
+  exit ${PUBLISH_ARTIFACTORY_FAILURE}
 fi
 
-exit $SUCCESS
+exit ${SUCCESS}
