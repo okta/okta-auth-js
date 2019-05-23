5589c7b1843e41fa21b3e3fd269c8d60838b8f05
Aaron Granick <aaron.granick@okta.com>
2019-05-22 18:16:32 -0700

    add method to test for PKCE support (+tests for features)

---
+++ test/spec/features.js
@@ -0,0 +1,90 @@
+var OktaAuth = require('OktaAuth');
+
+fdescribe('features', function() {
+
+  describe('functions exist', function() {
+    var funcs = [
+      'isPopupPostMessageSupported',
+      'isTokenVerifySupported',
+      'isPKCESupported',
+    ]
+
+    it('on prototype', function() {
+      var auth = new OktaAuth({
+        issuer: 'https://fakeo'
+      });
+
+      funcs.forEach(function(fname)  {
+        var fn = auth.features[fname];
+        expect(typeof fn).toBe('function');
+      });
+    });
+  
+    it('on static type', function()  {
+      funcs.forEach(function(fname) {
+        var fn = OktaAuth.features[fname];
+        expect(typeof fn).toBe('function');
+      });
+    });
+  });
+
+  describe('isPopupPostMessageSupported', function() {
+
+    it('can succeed', function() {
+      expect(OktaAuth.features.isPopupPostMessageSupported()).toBe(true);
+    })
+
+    it('not supported in IE < 10', function() {
+      document.documentMode = 9;
+      expect(OktaAuth.features.isPopupPostMessageSupported()).toBe(false);
+    })
+
+  });
+
+  describe('isTokenVerifySupported', function() {
+    beforeEach(function() {
+      window.crypto = {
+        subtle: true
+      };
+      window.Uint8Array = true;
+    });
+
+    it('can succeed', function() {
+      expect(OktaAuth.features.isTokenVerifySupported()).toBe(true);
+    })
+
+    it('fails if no crypto', function() {
+      window.crypto = undefined;
+      expect(OktaAuth.features.isTokenVerifySupported()).toBe(false);
+    });
+
+    it('fails if no Uint8Array', function() {
+      window.Uint8Array = undefined;
+      expect(OktaAuth.features.isTokenVerifySupported()).toBe(false);
+    });
+  });
+
+
+  describe('isPKCESupported', function() {
+    beforeEach(function() {
+      window.crypto = {
+        subtle: true
+      };
+      window.Uint8Array = true;
+    });
+
+    it('can succeed', function() {
+      expect(OktaAuth.features.isPKCESupported()).toBe(true);
+    })
+
+    it('fails if no crypto', function() {
+      window.crypto = undefined;
+      expect(OktaAuth.features.isPKCESupported()).toBe(false);
+    });
+
+    it('fails if no Uint8Array', function() {
+      window.Uint8Array = undefined;
+      expect(OktaAuth.features.isPKCESupported()).toBe(false);
+    });
+  });
+});
