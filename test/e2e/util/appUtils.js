a5c0f9c6d904940322da8f7ca669a08076f944f9
Aaron Granick <aaron.granick@okta.com>
2019-10-23 14:01:23 -0700

    WIP

--- test/e2e/util/appUtils.js
+++ test/e2e/util/appUtils.js
@@ -1,23 +1,33 @@
 import assert from 'assert';
 import TestApp from '../pageobjects/TestApp';
+import toQueryParams from './toQueryParams';
 
 const ISSUER = process.env.ISSUER;
 const CLIENT_ID = process.env.CLIENT_ID;
-
+const BASE_URL = 'http://localhost:8080';
 const flows = ['implicit', 'pkce'];
 
-function openImplicit() {
-  TestApp.open({ issuer: ISSUER, clientId: CLIENT_ID });
-  assert(TestApp.pkceOption.isSelected() === false);
-  assert(TestApp.issuer.getValue() === ISSUER);
-  assert(TestApp.clientId.getValue() === CLIENT_ID);
+function buildUrl(queryObj) {
+  return BASE_URL + toQueryParams(queryObj);
 }
 
-function openPKCE() {
-  TestApp.open({ issuer: ISSUER, clientId: CLIENT_ID, pkce: true });
-  assert(TestApp.pkceOption.isSelected());
-  assert(TestApp.issuer.getValue() === ISSUER);
-  assert(TestApp.clientId.getValue() === CLIENT_ID);
-}
+const PKCE_URL = buildUrl({ issuer: ISSUER, clientId: CLIENT_ID, pkce: true });
+const IMPLICIT_URL = buildUrl({ issuer: ISSUER, clientId: CLIENT_ID });
 
-export { flows, openImplicit, openPKCE };
+// function openImplicit() {
+//   TestApp.open({ issuer: ISSUER, clientId: CLIENT_ID });
+//   assert(TestApp.pkceOption.isSelected() === false);
+//   assert(TestApp.issuer.getValue() === ISSUER);
+//   assert(TestApp.clientId.getValue() === CLIENT_ID);
+// }
+
+// function openPKCE() {
+//   TestApp.open({ issuer: ISSUER, clientId: CLIENT_ID, pkce: true });
+//   assert(TestApp.pkceOption.isSelected());
+//   assert(TestApp.issuer.getValue() === ISSUER);
+//   assert(TestApp.clientId.getValue() === CLIENT_ID);
+// }
+
+
+
+export { flows, PKCE_URL, IMPLICIT_URL };
