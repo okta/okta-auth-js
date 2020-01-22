94ffd6b306ea0a9baf214a158ad6f236be554c2c
Aaron Granick <aaron.granick@okta.com>
2020-01-22 13:33:15 -0800

    test: add E2E test for server-side login (#312)

---
+++ test/app/env.js
@@ -0,0 +1,15 @@
+/* global process, __dirname */
+const dotenv = require('dotenv');
+const fs = require('fs');
+const path = require('path');
+const ROOT_DIR = path.resolve(__dirname, '..', '..');
+
+// Read environment variables from "testenv". Override environment vars if they are already set.
+const TESTENV = path.resolve(ROOT_DIR, 'testenv');
+
+if (fs.existsSync(TESTENV)) {
+  const envConfig = dotenv.parse(fs.readFileSync(TESTENV));
+  Object.keys(envConfig).forEach((k) => {
+    process.env[k] = envConfig[k];
+  });  
+}
