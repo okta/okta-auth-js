3c8d55d0b94eef3e3dc12be9c4b10bed556ce26e
jamesbui-okta <56932240+jamesbui-okta@users.noreply.github.com>
2020-07-23 13:14:44 -0700

    ci: update build scripts (#390)

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,14 +2,13 @@
 
 source ${OKTA_HOME}/${REPO}/scripts/setup.sh
 
-REGISTRY="${ARTIFACTORY_URL}/api/npm/npm-okta"
+REGISTRY="${ARTIFACTORY_URL}/api/npm/npm-topic"
 
 export TEST_SUITE_TYPE="build"
 
 # Install required dependencies
 export PATH="${PATH}:$(yarn global bin)"
-yarn global add @okta/ci-update-package
-yarn global add @okta/ci-pkginfo
+yarn global add @okta/ci-append-sha
 
 if [ -n "${action_branch}" ];
 then
@@ -25,23 +24,14 @@ cp ./*.md ./packages/okta-auth-js/
 
 pushd ./packages/okta-auth-js
 
-if ! ci-update-package --branch ${TARGET_BRANCH}; then
-  echo "ci-update-package failed! Exiting..."
+if ! ci-append-sha; then
+  echo "ci-append-sha failed! Exiting..."
   exit ${FAILED_SETUP}
 fi
 
-### looks like ci-update-package is not compatible with `yarn publish`
-### which expects new-version is passed via command line parameter.
-### keep using npm for now
+npm config set @okta:registry ${REGISTRY}
 if ! npm publish --registry ${REGISTRY}; then
   echo "npm publish failed! Exiting..."
-  exit ${PUBLISH_ARTIFACTORY_FAILURE}
-fi
-
-
-DATALOAD=$(ci-pkginfo -t dataload)
-if ! artifactory_curl -X PUT -u ${ARTIFACTORY_CREDS} ${DATALOAD} -v -f; then
-  echo "artifactory_curl failed! Exiting..."
   exit ${PUBLISH_ARTIFACTORY_FAILURE}
 fi
 
