1a97c23f9f4f8e41572f5df83c0be44f2de54626
Aaron Granick <aaron.granick@okta.com>
2020-08-13 18:56:31 -0700

    fixes unit on bacon

--- scripts/unit.sh
+++ scripts/unit.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -xe
+#!/bin/bash -x
 
 # Install current version of Chrome
 setup_service google-chrome-stable 79.0.3945.88-1
@@ -9,7 +9,10 @@ export TEST_SUITE_TYPE="jsunit"
 export TEST_RESULT_FILE_DIR="${REPO}/build2/reports/unit"
 
 # build is required to run E2E tests
-yarn build
+if ! yarn build; then
+  echo "build failed! Exiting..."
+  exit ${TEST_FAILURE}
+fi
 
 if ! yarn test:report; then
   echo "unit failed! Exiting..."
