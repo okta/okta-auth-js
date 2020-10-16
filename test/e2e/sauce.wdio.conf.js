08dd48df3206feadb5383cf6ae6b63c5a79b05a9
Shuo Wu <wushuo2010@gmail.com>
2020-10-16 13:13:28 -0400

    set spec retry times to 3

--- test/e2e/sauce.wdio.conf.js
+++ test/e2e/sauce.wdio.conf.js
@@ -176,7 +176,7 @@ exports.config = {
     framework: 'mocha',
     //
     // The number of times to retry the entire specfile when it fails as a whole
-    // specFileRetries: 1,
+    specFileRetries: 3,
     //
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
