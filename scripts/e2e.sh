1667cb4612135d2bc44869bdb880886f3af5c53f
Aaron Granick <aaron.granick@okta.com>
2020-11-09 15:07:09 -0800

    run E2E tests on Bacon

---
+++ scripts/e2e.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -xe
+
+source ${OKTA_HOME}/${REPO}/scripts/setup.sh
+
+setup_service google-chrome-stable 83.0.4103.61-1
+
+export TEST_SUITE_TYPE="junit"
+export TEST_RESULT_FILE_DIR="${REPO}/build2/e2e"
+
+export ISSUER=https://samples-javascript.okta.com/oauth2/default
+export SPA_CLIENT_ID=0oapmwm72082GXal14x6
+export USERNAME=george@acme.com
+get_secret prod/okta-sdk-vars/password PASSWORD
+
+if ! yarn test:e2e; then
+  echo "e2e tests failed! Exiting..."
+  exit ${TEST_FAILURE}
+fi
+
+echo ${TEST_SUITE_TYPE} > ${TEST_SUITE_TYPE_FILE}
+echo ${TEST_RESULT_FILE_DIR} > ${TEST_RESULT_FILE_DIR_FILE}
+exit ${PUBLISH_TYPE_AND_RESULT_DIR}
