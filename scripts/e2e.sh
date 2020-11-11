dd3c4040fecdb231ece0e13f38c8f55dc5b37d2f
Vijet M <vijet.mahabaleshwar@okta.com>
2020-11-11 15:21:01 -0800

    Upgrades to chrome 85
    OKTA-344811
    <<<Jenkins Check-In of Tested SHA: ecb9de7569eb4cda5d86dda70d527f109b547a02 for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-auth-js

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -3,7 +3,7 @@
 source ${OKTA_HOME}/${REPO}/scripts/setup.sh
 
 setup_service java 1.8.222
-setup_service google-chrome-stable 83.0.4103.61-1
+setup_service google-chrome-stable 85.0.4183.102-1
 
 export TEST_SUITE_TYPE="junit"
 export TEST_RESULT_FILE_DIR="${REPO}/build2/e2e"
