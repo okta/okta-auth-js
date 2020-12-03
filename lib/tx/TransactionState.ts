fff22308a849022366b7123531312b947c776f5e
Shuo Wu <wushuo2010@gmail.com>
2020-12-03 14:23:09 -0500

    feat: add mfa types - OKTA-343852

--- lib/tx/TransactionState.ts
+++ lib/tx/TransactionState.ts
@@ -1,5 +1,12 @@
 export class TransactionState {
   stateToken?: string;
+  type?: string;
+  expiresAt?: string;
+  relayState?: string;
+  factorResult?: string;
+  factorType?: string;
+  recoveryToken?: string;
+  recoveryType?: string;
   autoPush?: boolean | Function;
   rememberDevice?: boolean | Function;
   profile?: {
