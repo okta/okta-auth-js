81437a48b4643d900c26dc65ea2293ee446c9df3
Vijet M <vijet.mahabaleshwar@okta.com>
2020-12-15 10:44:38 -0800

    Removes e2e tests from travis

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,11 +1,6 @@
 set -e
-# if this build was triggered via a cron job, run tests on sauce labs
-if [ "${TRAVIS_EVENT_TYPE}" = "cron" ] ; then
-    export RUN_SAUCE_TESTS=true;
-    yarn test:e2e
-else
-    # run the validate and e2e tests (on chrome headless)
-    # validate will run lint and typescript build
-    yarn validate
-    yarn test
-fi
+
+# run the validate and unit tests
+# validate will run lint and typescript build
+yarn validate
+yarn test:unit
