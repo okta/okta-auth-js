07b016b5005d465fceb1a7587d18160717c2b052
Aaron Granick <aaron.granick@okta.com>
2021-03-09 21:08:27 +0000

    Update widget to 5.4.2
    OKTA-376248
    <<<Jenkins Check-In of Tested SHA: 865d5114526218aa1eeeaf6a0208ce97fe6820ed for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-auth-js

--- samples/config.js
+++ samples/config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 // All samples use the same widget version, whether CDN or NPM
-const SIW_VERSION = '5.4.0';
+const SIW_VERSION = '5.4.2';
 
 const AUTH_JS_DIR = path.dirname(require.resolve('@okta/okta-auth-js'));
 const AUTH_JS_VERSION = require(path.resolve(AUTH_JS_DIR, '..', '..', 'package.json')).version;
