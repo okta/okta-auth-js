a4b1ac1be13e3f87471e446d68144b5d1fe10ba0
Denys Oblohin <denys.oblohin@okta.com>
2021-03-22 21:01:05 +0200

    moved types under test

--- scripts/tsd.sh
+++ scripts/tsd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 
-cp ./scripts/types/index.d.ts ./build/
-cp ./scripts/types/index.test-d.ts ./build/
-cp ./scripts/types/tsconfig.json ./build/
+cp ./test/types/index.d.ts ./build/
+cp ./test/types/index.test-d.ts ./build/
+cp ./test/types/tsconfig.json ./build/
 
 tsd
