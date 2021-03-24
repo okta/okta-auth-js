8537f456c14efee794b35749849f0bb87f439564
Aaron Granick <aaron.granick@okta.com>
2021-03-24 10:51:52 -0700

    fix karma

--- lib/crypto/browser.ts
+++ lib/crypto/browser.ts
@@ -1,6 +1,6 @@
 /* global atob, btoa, crypto */
-const a = atob;
-const b = btoa;
+const a = function(str) { return atob(str); };
+const b = function (str) { return btoa(str); };
 const c = crypto;
 
 export { a as atob, b as btoa, c as webcrypto };
