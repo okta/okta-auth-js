156f73c4d3c14181f1a8238c67781f65ee2d9550
Aaron Granick <aaron.granick@okta.com>
2021-04-02 13:17:05 -0700

    fix type test / yarn.lock

--- test/types/index.d.ts
+++ test/types/index.d.ts
@@ -10,4 +10,4 @@
  * See the License for the specific language governing permissions and limitations under the License.
  *
  */
-export * from '../../build/lib/browser';
+export * from '../../build/lib';
