a29968d4c3f574d159b082510546d3be72ab41ef
Shuo Wu <60160041+shuowu-okta@users.noreply.github.com>
2021-04-14 12:07:39 -0400

    Samples: add self hosted widget sample OKTA-382175 (#691)
    Co-authored-by: Shuo Wu <wushuo2010@gmail.com>
    OKTA-388545
    <<<Jenkins Check-In of Tested SHA: 6842f2abbe3be86efea7f3a05b6349e2e637fab4 for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-auth-js
    Files changed count: 252

--- samples/generated/express-direct-auth/web-server/routes/login.js
+++ samples/generated/express-direct-auth/web-server/routes/login.js
@@ -1,10 +1,63 @@
 const express = require('express');
 const { getAuthClient } = require('../utils');
+const sampleConfig = require('../../config').webServer;
 
 const router = express.Router();
 
-router.get('/login', (_, res) => {
+const renderLogin = (req, res) => {
   res.render('login');
+};
+
+const renderLoginWithWidget = (req, res) => {
+  const authClient = getAuthClient(req);
+  authClient.idx.interact()
+    .then(idxRes => {
+      const { 
+        state,
+        interactionHandle, 
+        meta: { codeChallenge, codeChallengeMethod } 
+      } = idxRes;
+      if (!interactionHandle) {
+        return res.render('login-with-widget', {
+          hasError: true,
+          errors: ['Missing required congifuration "interactionHandle" to initial the widget.'],
+        });
+      }
+      const widgetConfig = JSON.stringify({
+        baseUrl: sampleConfig.oidc.issuer.split('/oauth2')[0],
+        clientId: sampleConfig.oidc.clientId,
+        redirectUri: sampleConfig.oidc.redirectUri,
+        authParams: {
+          issuer: sampleConfig.oidc.issuer,
+          scopes: sampleConfig.oidc.scope.split(' '),
+        },
+        useInteractionCodeFlow: true,
+        state,
+        interactionHandle,
+        codeChallenge,
+        codeChallengeMethod,
+      });
+      res.render('login-with-widget', {
+        siwVersion: '5.5.2',
+        widgetConfig,
+      });
+    })
+    .catch(err => {
+      console.log('Failed to render widget, error: ', err);
+      res.render('login-with-widget', {
+        hasError: true,
+        errors: ['Failed to render widget']
+      });
+    });
+};
+
+router.get('/login', (req, res) => {
+  const { widget } = req.query;
+  if (widget) {
+    renderLoginWithWidget(req, res);
+  } else {
+    renderLogin(req, res); 
+  }
 });
 
 router.post('/login', async (req, res) => {
@@ -12,14 +65,12 @@ router.post('/login', async (req, res) => {
 
   try {
     // Get tokens and userInfo
-    const authClient = getAuthClient();
+    const authClient = getAuthClient(req);
     const { data: 
       { tokens: { tokens } } 
     } = await authClient.idx.authenticate({ username, password });
-    const { accessToken, idToken } = tokens;
-    const userinfo = await authClient.token.getUserInfo(accessToken, idToken);
-    // Persist userContext in session
-    req.session.userContext = JSON.stringify({ userinfo, tokens });
+    // Save tokens to storage (req.session)
+    authClient.tokenManager.setTokens(tokens);
     // Redirect back to home page
     res.redirect('/');
   } catch (err) {
@@ -30,6 +81,24 @@ router.post('/login', async (req, res) => {
       hasError: errors && errors.length,
       errors 
     });
+  }
+});
+
+router.get('/login/callback', async (req, res) => {
+  const { interaction_code: interactionCode } = req.query;
+  try {
+    // Exchange code for tokens
+    const authClient = getAuthClient(req);
+    const meta = authClient.transactionManager.load();
+    const { codeVerifier } = meta;
+    const { tokens } = await authClient.token.exchangeCodeForTokens({ interactionCode, codeVerifier });
+    // Save tokens to storage (req.session)
+    authClient.tokenManager.setTokens(tokens);
+    // Redirect back to home page
+    res.redirect('/');
+  } catch (err) {
+    console.log('Failed to handle interaction code callback, error: ', err);
+    res.redirect('/login?widget=1');
   }
 });
 
