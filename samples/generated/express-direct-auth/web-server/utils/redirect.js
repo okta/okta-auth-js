7675fb8f398f1d6c9f6c01637cc118235f243e48
Shuo Wu <wushuo2010@gmail.com>
2021-04-27 17:17:24 -0400

    use state instead of transactionId

--- samples/generated/express-direct-auth/web-server/utils/redirect.js
+++ samples/generated/express-direct-auth/web-server/utils/redirect.js
@@ -1,4 +1,9 @@
 module.exports = function redirect({ req, res, path }) {
-  const { transactionId } = req.query;
-  res.redirect(`${path}?transactionId=${transactionId}`);
+  const { transactionId } = req;
+
+  const url = new URL(path, 'relative:///');
+  url.searchParams.set('state', transactionId);
+  
+  const redirectPath = `${url.pathname}${url.search}${url.hash}`;
+  res.redirect(redirectPath);
 };
--- samples/templates/express-direct-auth/web-server/utils/getFormActionPath.js
+++ samples/templates/express-direct-auth/web-server/utils/getFormActionPath.js
@@ -2,7 +2,7 @@ module.exports = function getFormActionPath(req, path) {
   const { transactionId } = req;
 
   const url = new URL(path, 'relative:///');
-  url.searchParams.set('transactionId', transactionId);
+  url.searchParams.set('state', transactionId);
   
   return `${url.pathname}${url.search}${url.hash}`;
 };
