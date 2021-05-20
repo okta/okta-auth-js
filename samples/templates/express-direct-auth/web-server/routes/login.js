a158e2a3608a2d05e3e56fdea12b25a128bcdd94
Shuo Wu <wushuo2010@gmail.com>
2021-05-20 17:02:09 -0400

    feat: social idp support in sample

--- samples/templates/express-direct-auth/web-server/routes/login.js
+++ samples/templates/express-direct-auth/web-server/routes/login.js
@@ -7,6 +7,15 @@ const {
 
 const router = express.Router();
 
+const getIdpSemanticClass = (type) => {
+  switch (type) {
+    case 'GOOGLE':
+      return 'google plus';
+    default: 
+    return '';
+  }
+}
+
 // entry route
 router.get('/login', async (req, res) => {
   req.session.idxMethod = 'authenticate';
@@ -17,7 +26,7 @@ router.get('/login', async (req, res) => {
   const idps = availableSteps 
     ? availableSteps
       .filter(({ name }) => name === 'redirect-idp')
-      .map(({ href, idp: { name } }) => ({ name, href })) 
+      .map(({ href, idp: { name }, type }) => ({ name, href, class: getIdpSemanticClass(type) })) 
     : [];
 
   const { authenticator } = req.query;
@@ -40,6 +49,25 @@ router.post('/login', async (req, res, next) => {
     authenticators: authenticator ? [authenticator] : [],
   });
   handleTransaction({ req, res, next, authClient, transaction });
+});
+
+router.get('/login/callback', async (req, res, next) => {
+  const url = req.protocol + '://' + req.get('host') + req.originalUrl;
+  const authClient = getAuthClient(req);
+  try {
+    // Exchange code for tokens
+    await authClient.idx.handleInteractionCodeRedirect(url);
+    // Redirect back to home page
+    res.redirect('/');
+  } catch (err) {
+    if (authClient.isInteractionRequiredError(err) === true) {
+      const error = new Error('Multifactor Authentication and Social Identity Providers is not currently supported, Authentication failed.');
+      next(error);
+      return;
+    }
+
+    next(err);
+  }
 });
 
 module.exports = router;
