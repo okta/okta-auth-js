802788774587ab6b049b5b6a16333a80009dfafc
Felix Schwarzmeier <felixsla21@gmail.com>
2021-05-25 23:15:09 +0200

    Update api.ts
    Fix ParseFromUrlFunction parameter type

--- lib/types/api.ts
+++ lib/types/api.ts
@@ -127,7 +127,7 @@ export interface ParseFromUrlOptions {
   responseMode?: string;
 }
 
-export type ParseFromUrlFunction = () => Promise<TokenResponse>;
+export type ParseFromUrlFunction = (options?: string | ParseFromUrlOptions) => Promise<TokenResponse>;
 
 export interface ParseFromUrlInterface extends ParseFromUrlFunction {
   _getDocument: () => Document;
