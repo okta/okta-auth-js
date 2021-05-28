5451357b8442189a142f4b3e9fd226c953cad8ba
denys.oblohin <denys.oblohin@okta.om>
2021-05-28 12:23:31 +0300

    OIE e2e Scenario 1.1.3

--- samples/test/steps/when.ts
+++ samples/test/steps/when.ts
@@ -26,7 +26,7 @@ When(
 );
 
 When(
-  /^she fills in her (incorrect username|username|password|correct password)$/,
+  /^she fills in her (incorrect username|correct username|username|incorrect password|password|correct password)$/,
   enterCredential
 );
 
@@ -36,7 +36,7 @@ When(
 );
 
 When(
-  /^she submits the Login form$/,
+  /^she submits the Login form(?: with blank fields)?$/,
   submitForm
 );
 
