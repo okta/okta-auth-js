e3e19810768e7acb6954145c888d3e696cf2f2d5
Shuo Wu <wushuo2010@gmail.com>
2021-06-14 23:35:36 +0000

    embedded auth sample error handling issues - OKTA-401417
    OKTA-401417
    <<<Jenkins Check-In of Tested SHA: 288e5ccdcac139491901c1719cc63f0cc4ffdbeb for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-auth-js
    Files changed count: 6
    PR Link: "https://github.com/okta/okta-auth-js/pull/826"

--- samples/templates/express-embedded-auth-with-sdk/web-server/routes/home.js
+++ samples/templates/express-embedded-auth-with-sdk/web-server/routes/home.js
@@ -3,6 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+  // clear any existing transaction
+  req.clearFlowStates();
+
+  // collect attrs for render
   const userinfo = req.userContext && req.userContext.userinfo;
   const attributes = userinfo ? Object.entries(userinfo) : [];
   res.render('home', {
