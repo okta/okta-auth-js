5cb02eb6b8e3c8594a33b91b9922d2253bce5c8a
denys.oblohin <denys.oblohin@okta.om>
2021-07-27 19:19:36 +0300

    Restore test:specs

--- samples/test/scripts/run.sh
+++ samples/test/scripts/run.sh
@@ -2,7 +2,7 @@
 
 # TODO: enable specs for spa/web apps after figuring out how to properly map org/policy to specs
 # Test specs/*
-# yarn test:specs
+yarn test:specs
 
 # Test features/*
 yarn test:features
