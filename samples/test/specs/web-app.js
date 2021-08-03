ea00e0476aff98a2ef79888f0ce567967af6e433
denys.oblohin <denys.oblohin@okta.om>
2021-08-03 19:34:27 +0300

    enable web test

--- samples/test/specs/web-app.js
+++ samples/test/specs/web-app.js
@@ -19,16 +19,16 @@ const sampleConfig = getSampleConfig();
 describe('web-app: ' + sampleConfig.name, () => {
 
   // TODO: fix this test. Fails in incognito, succeeds in regular window. May be related to 3rd party cookies.
-  // xit('can login directly, calling signin() with username and password', async () => {
-  //   await startApp(WebApp);
-  //   await loginDirect(WebApp);
-  //   if (sampleConfig.oidc) {
-  //     await browser.pause(5000);
-  //     await WebApp.assertAccessToken();
-  //   } else {
-  //     await WebApp.assertLoginSuccess();
-  //   }
-  // });
+  it('can login directly, calling signin() with username and password', async () => {
+    await startApp(WebApp);
+    await loginDirect(WebApp);
+    if (sampleConfig.oidc) {
+      await browser.pause(5000);
+      await WebApp.assertAccessToken();
+    } else {
+      await WebApp.assertLoginSuccess();
+    }
+  });
 
   it('will show error if wrong password is provided', async () => {
     await startApp(WebApp);
