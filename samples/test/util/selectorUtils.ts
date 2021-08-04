06af0dbcb4b87ea2040c88f9ec914c85d414ba56
denys.oblohin <denys.oblohin@okta.om>
2021-08-04 18:04:15 +0300

    nit

--- samples/test/util/selectorUtils.ts
+++ samples/test/util/selectorUtils.ts
@@ -1,8 +1,7 @@
 import { OktaSignInV1, OktaSignInOIE } from  '../support/selectors';
 
-function getOktaSignInForm(useInteractionCodeFlow?: boolean) {
-  const useOIE = useInteractionCodeFlow != undefined ? useInteractionCodeFlow : process.env.ORG_OIE_ENABLED;
-  return useOIE ? OktaSignInOIE : OktaSignInV1;
+function getOktaSignInForm() {
+  return process.env.ORG_OIE_ENABLED ? OktaSignInOIE : OktaSignInV1;
 }
 
 export { getOktaSignInForm };
