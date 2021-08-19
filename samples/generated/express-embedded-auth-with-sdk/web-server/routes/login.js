87ee7269f24414aaf8565bbcb73026e2cd8b18bf
denys.oblohin <denys.oblohin@okta.om>
2021-08-20 02:08:11 +0300

    Protect router methods with try/catch wrapper

--- samples/generated/express-embedded-auth-with-sdk/web-server/routes/login.js
+++ samples/generated/express-embedded-auth-with-sdk/web-server/routes/login.js
@@ -11,14 +11,14 @@
  */
 
 
-const express = require('express');
 const { 
   getAuthClient, 
   handleTransaction,
   renderTemplate,
+  routerWithCatch,
 } = require('../utils');
 
-const router = express.Router();
+const router = routerWithCatch();
 
 const getIdpSemanticClass = (type) => {
   switch (type) {
