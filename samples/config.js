b52c097b975325398bd0780e5538ef152678d798
Oleksandr Pravosudko <oleksandr.pravosudko@okta.com>
2021-08-25 10:57:37 +0300

    enable all features

--- samples/config.js
+++ samples/config.js
@@ -77,6 +77,14 @@ const samples = [
     specs: [],
     features: [
       'root-page', 
+      'basic-auth', 
+      'self-service-password-recovery', 
+      'self-service-registration',
+      'self-service-registration-custom-attribute',
+      'mfa-password-and-email',
+      'mfa-password-and-sms',
+      'social-login-mfa',
+      'social-idp'
     ],
     useEnv: true
   },
