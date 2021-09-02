9d161b3a2d78a1f50326d40cb26ff141f37cab66
Seyi <seyi@outlook.com>
2021-09-01 23:22:22 -0700

    fix: autoRenew tokens when isAuthenticated is called tokenManager config is not copied from OktaAuth instantiation options and not available in OktaAuth.isAuthenticated Resolves: #924

--- lib/options.ts
+++ lib/options.ts
@@ -145,6 +145,7 @@ export function buildOptions(args: OktaAuthOptions = {}): OktaAuthOptions {
     devMode: !!args.devMode,
     storageManager: args.storageManager,
     cookies: isBrowser() ? getCookieSettings(args, isHTTPS()) : args.cookies,
+    tokenManager: args.tokenManager,
 
     // Give the developer the ability to disable token signature validation.
     ignoreSignature: !!args.ignoreSignature,
