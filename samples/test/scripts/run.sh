b3b8d334bf243147234bd77a3269b5f1aa6fd35b
Yuliia Lysenko <yuliia.lysenko@okta.com>
2021-10-21 12:26:02 +0300

    delete run test specs command from scripts/run.sh

--- samples/test/scripts/run.sh
+++ samples/test/scripts/run.sh
@@ -1,11 +1,5 @@
 #!/bin/bash -x
 
-# Test specs/*
-if ! yarn test:specs; then
-  echo "Specs tests failed! Exiting..."
-  exit ${TEST_FAILURE}
-fi
-
 # Test features/*
 if ! yarn test:features; then
   echo "Features tests failed! Exiting..."
