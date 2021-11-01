4e6fc59d486710ddd9f38f6c0f0ff08b2c1e1912
Vijet M <vijet.mahabaleshwar@okta.com>
2021-11-01 14:59:44 -0700

    Uses okta IdP for social login MFA test

--- scripts/e2e-samples.sh
+++ scripts/e2e-samples.sh
@@ -19,9 +19,6 @@ get_vault_secret_key devex/auth-js-sdk-vars a18n_api_key A18N_API_KEY
 export FB_USERNAME=js_ekdtypn_user@tfbnw.net
 get_secret prod/okta-sdk-vars/fb_password FB_PASSWORD
 
-# Pull testenv.yml file
-# aws s3 --quiet --region us-east-1 cp s3://ci-secret-stash/prod/okta-sdk-vars/testenv.yml $OKTA_HOME/$REPO/testenv.yml
-
 # If this script is run as a bacon task, run against trexcloud environment
 if [[ "${BACON_TASK}" == true ]]; then
   echo "Running tests against trexcloud org"
@@ -39,25 +36,12 @@ else
   export ISSUER=https://javascript-idx-sdk.okta.com/oauth2/default
   export CLIENT_ID=0oav2oxnlYjULp0Cy5d6
   export SPA_CLIENT_ID=0oa17suj5x9khaVH75d7
-  export WEB_CLIENT_ID=0oav2oxnlYjULp0Cy5d6
-  get_vault_secret_key devex/prod-js-idx-sdk-vars prod_client_secret WEB_CLIENT_SECRET
   get_vault_secret_key devex/prod-js-idx-sdk-vars prod_client_secret CLIENT_SECRET
   get_vault_secret_key devex/prod-js-idx-sdk-vars prod_idx_sdk_org_api_key OKTA_API_KEY
   get_vault_secret_key devex/prod-js-idx-sdk-vars prod_mfa_client_id MFA_CLIENT_ID
   get_vault_secret_key devex/prod-js-idx-sdk-vars prod_mfa_client_secret MFA_CLIENT_SECRET
   get_vault_secret_key devex/prod-js-idx-sdk-vars prod_custom_client_id CUSTOM_CLIENT_ID
   get_vault_secret_key devex/prod-js-idx-sdk-vars prod_custom_client_secret CUSTOM_CLIENT_SECRET
-
-  # echo "Running tests against trexcloud org"
-  # export ISSUER=https://javascript-idx-sdk.trexcloud.com/oauth2/default
-  # export CLIENT_ID=0oa3r1keeeFFb7VMG0g7
-  # export SPA_CLIENT_ID=0oa3r92jj01DWBeWC0g7
-  # get_vault_secret_key devex/trex-js-idx-sdk-vars trex_client_secret CLIENT_SECRET
-  # get_vault_secret_key devex/trex-js-idx-sdk-vars trex_idx_sdk_org_api_key OKTA_API_KEY
-  # get_vault_secret_key devex/trex-js-idx-sdk-vars trex_mfa_client_id MFA_CLIENT_ID
-  # get_vault_secret_key devex/trex-js-idx-sdk-vars trex_mfa_client_secret MFA_CLIENT_SECRET
-  # get_vault_secret_key devex/trex-js-idx-sdk-vars trex_custom_client_id CUSTOM_CLIENT_ID
-  # get_vault_secret_key devex/trex-js-idx-sdk-vars trex_custom_client_secret CUSTOM_CLIENT_SECRET
 fi
 
 sed "s|REPLACE_ISSUER|"$ISSUER"|g;s|REPLACE_DEFAULT_CLIENT_ID|"$CLIENT_ID"|g; \
