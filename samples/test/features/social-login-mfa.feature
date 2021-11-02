819d92883fb0b9e2abcbdbc43fc73dcbcf9ea42a
Vijet M <vijet.mahabaleshwar@okta.com>
2021-11-02 21:55:11 +0000

    Adds testenv.yml template and values in vault
    OKTA-436210
    <<<Jenkins Check-In of Tested SHA: 4e6fc59d486710ddd9f38f6c0f0ff08b2c1e1912 for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-auth-js
    Files changed count: 3
    PR Link: "https://github.com/okta/okta-auth-js/pull/993"

--- samples/test/features/social-login-mfa.feature
+++ samples/test/features/social-login-mfa.feature
@@ -2,16 +2,12 @@ Feature: Direct Auth Social Login with MFA
   
   Background:
     Given a SPA, WEB APP or MOBILE Policy that defines Password as the only factor required for authentication
-    # And a configured IDP object for Facebook
-    # And an IDP routing rule defined to allow users in the Sample App to use the IDP
-      And the Application Sign on Policy is set to "Password + Another Factor"
+    And the Application Sign on Policy is set to "Password + Another Factor"
     And a user named "Mary"
-    # And Mary does not have an account in the org
 
-  Scenario: Mary logs in with a social IDP and gets an error message
-    Given Mary navigates to Basic Social Login View
-    When she clicks the "Login with Facebook" button
-    And logs in to Facebook
+  Scenario: Mary logs in with a Okta OIDC IDP and gets an error message
+    Given Mary navigates to the Login View
+    When she clicks the "Login with Okta OIDC IDP" button
+    And logs in to Okta OIDC IDP
     # And the remediation returns "MFA_REQUIRED"
-    Then she should see an error message "Multifactor Authentication and Social Identity Providers is not currently supported, Authentication failed." 
-  
+    # Then she should see an error message "Multifactor Authentication and Social Identity Providers is not currently supported, Authentication failed."   
--- scripts/e2e-samples.sh
+++ scripts/e2e-samples.sh
@@ -10,7 +10,6 @@ export TEST_RESULT_FILE_DIR="${REPO}/build2/reports/e2e"
 export CI=true
 export DBUS_SESSION_BUS_ADDRESS=/dev/null
 
-
 # Configuration
 # Remember to set this while running tests locally 
 export ORG_OIE_ENABLED=true 
@@ -20,11 +19,6 @@ get_vault_secret_key devex/auth-js-sdk-vars a18n_api_key A18N_API_KEY
 export FB_USERNAME=js_ekdtypn_user@tfbnw.net
 get_secret prod/okta-sdk-vars/fb_password FB_PASSWORD
 
-# Pull testenv.yml file
-aws s3 --quiet --region us-east-1 cp s3://ci-secret-stash/prod/okta-sdk-vars/testenv.yml $OKTA_HOME/$REPO/testenv.yml
-#get_secret prod/okta-sdk-vars/testenv.yml TESTENV_YML
-#echo $TESTENV_YML > $OKTA_HOME/$REPO/testenv.yml
-
 # If this script is run as a bacon task, run against trexcloud environment
 if [[ "${BACON_TASK}" == true ]]; then
   echo "Running tests against trexcloud org"
@@ -32,15 +26,30 @@ if [[ "${BACON_TASK}" == true ]]; then
   export CLIENT_ID=0oa3r1keeeFFb7VMG0g7
   export SPA_CLIENT_ID=0oa3r92jj01DWBeWC0g7
   get_vault_secret_key devex/trex-js-idx-sdk-vars trex_client_secret CLIENT_SECRET
-  get_vault_secret_key devex/trex-js-idx-sdk-vars trex_idx_sdk_e2e_apiKey OKTA_API_KEY
+  get_vault_secret_key devex/trex-js-idx-sdk-vars trex_idx_sdk_org_api_key OKTA_API_KEY
+  get_vault_secret_key devex/trex-js-idx-sdk-vars trex_mfa_client_id MFA_CLIENT_ID
+  get_vault_secret_key devex/trex-js-idx-sdk-vars trex_mfa_client_secret MFA_CLIENT_SECRET
+  get_vault_secret_key devex/trex-js-idx-sdk-vars trex_mfa_client_id CUSTOM_CLIENT_ID
+  get_vault_secret_key devex/trex-js-idx-sdk-vars trex_mfa_client_secret CUSTOM_CLIENT_SECRET
 else
   echo "Running tests against production (ok12) org"
-  export SPA_CLIENT_ID=0oa17suj5x9khaVH75d7
   export ISSUER=https://javascript-idx-sdk.okta.com/oauth2/default
   export CLIENT_ID=0oav2oxnlYjULp0Cy5d6
-  get_vault_secret_key devex/js-idx-sdk-vars client_secret CLIENT_SECRET
-  get_secret prod/okta-sdk-vars/idx_sdk_e2e_apiKey OKTA_API_KEY
+  export SPA_CLIENT_ID=0oa17suj5x9khaVH75d7
+  get_vault_secret_key devex/prod-js-idx-sdk-vars prod_client_secret CLIENT_SECRET
+  get_vault_secret_key devex/prod-js-idx-sdk-vars prod_idx_sdk_org_api_key OKTA_API_KEY
+  get_vault_secret_key devex/prod-js-idx-sdk-vars prod_mfa_client_id MFA_CLIENT_ID
+  get_vault_secret_key devex/prod-js-idx-sdk-vars prod_mfa_client_secret MFA_CLIENT_SECRET
+  get_vault_secret_key devex/prod-js-idx-sdk-vars prod_custom_client_id CUSTOM_CLIENT_ID
+  get_vault_secret_key devex/prod-js-idx-sdk-vars prod_custom_client_secret CUSTOM_CLIENT_SECRET
 fi
+
+sed "s|REPLACE_ISSUER|"$ISSUER"|g;s|REPLACE_DEFAULT_CLIENT_ID|"$CLIENT_ID"|g; \
+     s|REPLACE_DEFAULT_CLIENT_SECRET|"$CLIENT_SECRET"|g;s|REPLACE_USERNAME|"$USERNAME"|g; \
+     s|REPLACE_PASSWORD|"$PASSWORD"|g;s|REPLACE_A18N_API_KEY|"$A18N_API_KEY"|g; \ 
+     s|REPLACE_OKTA_API_KEY|"$OKTA_API_KEY"|g;s|REPLACE_MFA_CLIENT_ID|"$MFA_CLIENT_ID"|g; \
+     s|REPLACE_MFA_CLIENT_SECRET|"$MFA_CLIENT_SECRET"|g;s|REPLACE_CUSTOM_CLIENT_ID|"$CUSTOM_CLIENT_ID"|g; \
+     s|REPLACE_CUSTOM_CLIENT_SECRET|"$CUSTOM_CLIENT_SECRET"|g;" $OKTA_HOME/$REPO/_testenv.yml > $OKTA_HOME/$REPO/testenv.yml
 
 # Run the tests
 if ! yarn test:samples; then
