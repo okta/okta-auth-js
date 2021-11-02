afa7e9deadf3475f41b5f45b88817e3c158bb9e6
Yuliia Lysenko <yuliia.lysenko@okta.com>
2021-11-02 12:17:18 +0200

    add e2e-saucelabs.sh

---
+++ scripts/e2e-saucelabs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -x
+
+source ${OKTA_HOME}/${REPO}/scripts/setup.sh
+
+setup_service java 1.8.222
+setup_service google-chrome-stable 89.0.4389.72-1
+
+# run e2e tests with test/e2e/sauce.wdio.conf.js config
+export RUN_SAUCE_TESTS=true
+
+export TEST_SUITE_TYPE="junit"
+export TEST_RESULT_FILE_DIR="${REPO}/build2/reports/e2e"
+export CI=true
+export DBUS_SESSION_BUS_ADDRESS=/dev/null
+
+echo "Running tests against production (ok12) org"
+export ISSUER=https://javascript-idx-sdk.okta.com/oauth2/default
+export CLIENT_ID=0oav2oxnlYjULp0Cy5d6
+
+export USERNAME=mary@acme.com
+get_secret prod/okta-sdk-vars/password PASSWORD
+get_vault_secret_key devex/js-idx-sdk-vars client_secret CLIENT_SECRET
+get_secret prod/okta-sdk-vars/idx_sdk_e2e_apiKey OKTA_API_KEY
+
+# Run the tests
+if ! yarn test:e2e; then
+  echo "MFA e2e tests failed! Exiting..."
+  exit ${TEST_FAILURE}
+fi
+
+echo ${TEST_SUITE_TYPE} > ${TEST_SUITE_TYPE_FILE}
+echo ${TEST_RESULT_FILE_DIR} > ${TEST_RESULT_FILE_DIR_FILE}
+exit ${PUBLISH_TYPE_AND_RESULT_DIR}
