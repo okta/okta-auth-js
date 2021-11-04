b9bb6a765e09a5ae3667c8a333906086e8208643
Yuliia Lysenko <yuliia.lysenko@okta.com>
2021-11-04 15:10:58 +0200

    delete REFRESH_TOKEN, ORG_OIE_ENABLED

--- scripts/e2e-saucelabs.sh
+++ scripts/e2e-saucelabs.sh
@@ -12,9 +12,6 @@ export RUN_SAUCE_TESTS=true
 export SAUCE_USERNAME=OktaSignInWidget
 get_vault_secret_key devex/sauce-labs accessKey SAUCE_ACCESS_KEY
 
-export REFRESH_TOKEN=true
-export ORG_OIE_ENABLED=false
-
 export TEST_SUITE_TYPE="junit"
 export TEST_RESULT_FILE_DIR="${REPO}/build2/reports/e2e"
 export CI=true
