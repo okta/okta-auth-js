016d7a08d0aa3cf150bfbb3bf84e44167207ce0a
Yuliia Lysenko <yuliia.lysenko@okta.com>
2021-11-08 14:23:45 +0200

    increase maxInstances number in sauce.wdio.conf.js

--- test/e2e/sauce.wdio.conf.js
+++ test/e2e/sauce.wdio.conf.js
@@ -86,7 +86,7 @@ exports.config = {
     // and 30 processes will get spawned. The property handles how many capabilities
     // from the same test should run tests.
     //
-    maxInstances: 40,
+    maxInstances: 50,
     //
     // If you have trouble getting all important capabilities together, check out the
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
