9e9b28f15341a606403da773ab52b9f6158c7312
Yuliia Lysenko <yuliia.lysenko@okta.com>
2021-11-18 15:51:23 +0200

    update e2e-saucelabs.sh

--- scripts/e2e-saucelabs.sh
+++ scripts/e2e-saucelabs.sh
@@ -5,22 +5,22 @@ source ${OKTA_HOME}/${REPO}/scripts/setup.sh
 setup_service java 1.8.222
 setup_service google-chrome-stable 89.0.4389.72-1
 
-export TRAVIS=true # work-around to run tests on saucelabs instead of chrome
-
-# run e2e tests with test/e2e/sauce.wdio.conf.js config
-export RUN_SAUCE_TESTS=true
-export SAUCE_USERNAME=OktaSignInWidget
-get_vault_secret_key devex/sauce-labs accessKey SAUCE_ACCESS_KEY
-
 export TEST_SUITE_TYPE="junit"
 export TEST_RESULT_FILE_DIR="${REPO}/build2/reports/e2e-saucelabs"
 
 echo "Running tests against production (ok12) org"
 export ISSUER=https://samples-javascript.okta.com/oauth2/default
-export CLIENT_ID=0oa1xyzajgPFGWlLP4x7
 export USERNAME=george@acme.com
 get_secret prod/okta-sdk-vars/password PASSWORD
-get_secret prod/okta-sdk-vars/idx_sdk_e2e_apiKey OKTA_API_KEY
+
+export CI=true
+export DBUS_SESSION_BUS_ADDRESS=/dev/null
+
+# run e2e tests with test/e2e/sauce.wdio.conf.js config
+export RUN_SAUCE_TESTS=true
+export SAUCE_USERNAME=OktaSignInWidget
+export CLIENT_ID=0oa1xyzajgPFGWlLP4x7
+get_vault_secret_key devex/sauce-labs accessKey SAUCE_ACCESS_KEY
 
 # Run the tests
 if ! yarn test:e2e; then
