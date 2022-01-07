10da84d858dce326d956b88400a73d562ba6399c
Shuo Wu <wushuo2010@gmail.com>
2022-01-07 15:21:07 +0000

    BREAKING CHANGE: bump min node version to 12.20
    OKTA-407553
    <<<Jenkins Check-In of Tested SHA: 11252537fe9810db6659d4cd1b8380facbc81712 for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-auth-js
    Files changed count: 6
    PR Link: "https://github.com/okta/okta-auth-js/pull/1049"

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,7 +5,7 @@ export PATH="${PATH}:$(yarn global bin)"
 
 # Install required node version
 export NVM_DIR="/root/.nvm"
-setup_service node v12.13.0
+setup_service node v12.20.0
 
 cd ${OKTA_HOME}/${REPO}
 
