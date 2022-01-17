aebbd9d3a092ab14c510d0c0561495dd57d4d098
Shuo Wu <wushuo2010@gmail.com>
2022-01-17 12:42:50 -0500

    remove browser field

--- scripts/build.js
+++ scripts/build.js
@@ -68,7 +68,7 @@ packageJSON.private = false;
 packageJSON.scripts.prepare = '';
 
 // Remove "build/" from the entrypoint paths.
-['main', 'module', 'browser', 'types'].forEach(function(key) {
+['main', 'module', 'types'].forEach(function(key) {
   const value = packageJSON[key];
   if (typeof value === 'object' && value !== null) {
     packageJSON[key] = Object.keys(value).reduce((acc, curr) => {
