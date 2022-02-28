2adda804839f9d188b5b4a9524eaf0707191d15c
Shuo Wu <wushuo2010@gmail.com>
2022-02-28 17:52:46 +0000

    feat(idx): auto select authenticator data method
    OKTA-472634
    <<<Jenkins Check-In of Tested SHA: 29f1217b900eb3db75bc413c5b60219a64f9767a for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-auth-js
    Files changed count: 6
    PR Link: "https://github.com/okta/okta-auth-js/pull/1125"

--- lib/idx/remediators/AuthenticatorVerificationData.ts
+++ lib/idx/remediators/AuthenticatorVerificationData.ts
@@ -12,7 +12,9 @@
  */
 
 
+import { AuthSdkError } from '../../errors';
 import { AuthenticatorData, AuthenticatorDataValues } from './Base/AuthenticatorData';
+import { IdxRemediation } from '../types/idx-js';
 
 export type AuthenticatorVerificationDataValues = AuthenticatorDataValues;
 
@@ -20,16 +22,49 @@ export class AuthenticatorVerificationData extends AuthenticatorData {
   static remediationName = 'authenticator-verification-data';
 
   values!: AuthenticatorVerificationDataValues;
+  shouldProceedWithEmailAuthenticator: boolean;
+
+  constructor(remediation: IdxRemediation, values: AuthenticatorDataValues = {}) {
+    super(remediation, values);
+
+    // TODO: extend this feature to all authenticators
+    this.shouldProceedWithEmailAuthenticator = this.authenticator.methods.length === 1 
+      && this.authenticator.methods[0].type === 'email';
+  }
+
+  canRemediate() {
+    // auto proceed if there is only one method
+    if (this.shouldProceedWithEmailAuthenticator) {
+      return true;
+    }
+    return super.canRemediate();
+  }
 
   mapAuthenticator() {
     const authenticatorData = this.getAuthenticatorData();
     const authenticatorFromRemediation = this.getAuthenticatorFromRemediation();
+
+    // auto proceed with the only methodType option
+    if (this.shouldProceedWithEmailAuthenticator) {
+      return authenticatorFromRemediation.form?.value.reduce((acc, curr) => {
+        if (curr.value) {
+          acc[curr.name] = curr.value;
+        } else if (curr.options) {
+          acc[curr.name] = curr.options![0].value;
+        } else {
+          throw new AuthSdkError(`Unsupported authenticator data type: ${curr}`);
+        }
+        return acc;
+      }, {});
+    }
+
+    // return based on user selection
     return { 
       id: authenticatorFromRemediation.form!.value
         .find(({ name }) => name === 'id')!.value,
       enrollmentId: authenticatorFromRemediation.form!.value
         .find(({ name }) => name === 'enrollmentId')?.value,
-      methodType: authenticatorData!.methodType,
+      methodType: authenticatorData?.methodType,
     };
   }
 
