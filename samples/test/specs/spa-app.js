c588d61d04c2dffc7871230918e8c407c56ec60e
Jared Perreault <jared.perreault@okta.com>
2022-03-08 16:45:21 -0500

    chore: upgrade okta-sdk-nodejs to latest

--- samples/test/specs/spa-app.js
+++ samples/test/specs/spa-app.js
@@ -49,8 +49,8 @@ describe('spa-app: ' + sampleConfig.name, () => {
   });
 
   // TODO: fix this flaky test OKTA-464122
-  // eslint-disable-next-line jasmine/no-disabled-tests
-  xit('can use memory token storage', async () => {
+  // [UPDATE 3/8/22] could not get test to fail when attempt to repro. Re-enabling
+  it('can use memory token storage', async () => {
     await startApp('/', { authMethod: 'redirect', requireUserSession: true, storage: 'memory' });
     await loginRedirect();
     await checkProfile();
