4dfee80abe80328cc89ccaae144ecf161e43b0d0
Shuo Wu <wushuo2010@gmail.com>
2022-03-17 19:57:41 -0400

    remove e2e-samples bacon task
    replaced with e2e-express-embedded-auth-with-sdk

--- scripts/e2e-express-embedded-auth-with-sdk.sh
+++ scripts/e2e-express-embedded-auth-with-sdk.sh
@@ -12,6 +12,7 @@ export DBUS_SESSION_BUS_ADDRESS=/dev/null
 
 # Configuration
 # Remember to set this while running tests locally 
+SAMPLE_NAME=@okta/samples.express-embedded-auth-with-sdk
 export ORG_OIE_ENABLED=true 
 export USERNAME=mary@acme.com
 get_secret prod/okta-sdk-vars/password PASSWORD
