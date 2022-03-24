14e317967aa25d22366a4b8834d3535f9c30c2e4
Aaron Granick <aaron.granick@okta.com>
2022-03-24 13:24:37 -0700

    POC: publish multiple modules

---
+++ scripts/publish-sample.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -xe
+
+source ${OKTA_HOME}/${REPO}/scripts/setup.sh
+
+REGISTRY="${ARTIFACTORY_URL}/api/npm/npm-topic"
+
+export TEST_SUITE_TYPE="build"
+
+# Install required dependencies
+export PATH="${PATH}:$(yarn global bin)"
+yarn global add @okta/ci-append-sha
+
+if [ -n "${action_branch}" ];
+then
+  echo "Publishing from bacon task using branch ${action_branch}"
+  TARGET_BRANCH=${action_branch}
+else
+  echo "Publishing from bacon testSuite using branch ${BRANCH}"
+  TARGET_BRANCH=${BRANCH}
+fi
+
+pushd "./samples/generated/$ARTIFACT"
+
+sed -i "s/\"private\": true/\"private\": false/" package.json
+
+if ! ci-append-sha; then
+  echo "ci-append-sha failed! Exiting..."
+  exit ${FAILED_SETUP}
+fi
+
+npm config set @okta:registry ${REGISTRY}
+if ! npm publish --registry ${REGISTRY}; then
+  echo "npm publish failed! Exiting..."
+  exit ${PUBLISH_ARTIFACTORY_FAILURE}
+fi
+
+FINAL_PUBLISHED_VERSIONS=$(echo "console.log(require('./package.json').version)" | node -)
+log_custom_message "Published Version" "${FINAL_PUBLISHED_VERSIONS}"
+
+popd
+
+exit ${SUCCESS}
