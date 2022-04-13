5d3e9775b39bd1a1b6c6aa2976dfd9907bbcf5ad
Aaron Granick <aaron.granick@okta.com>
2022-04-13 22:17:15 +0000

    add upstream publish script
    OKTA-468777
    <<<Jenkins Check-In of Tested SHA: 879e7f3f413e4688ad9d8a86d4cfee82ee37b23e for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-auth-js
    Files changed count: 1
    PR Link: "https://github.com/okta/okta-auth-js/pull/1178"

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -9,6 +9,7 @@ export TEST_SUITE_TYPE="build"
 # Install required dependencies
 export PATH="${PATH}:$(yarn global bin)"
 yarn global add @okta/ci-append-sha
+yarn global add @okta/ci-pkginfo
 
 if [ -n "${action_branch}" ];
 then
@@ -33,8 +34,17 @@ if ! npm publish --registry ${REGISTRY}; then
   exit ${PUBLISH_ARTIFACTORY_FAILURE}
 fi
 
-FINAL_PUBLISHED_VERSIONS=$(echo "console.log(require('./package.json').version)" | node -)
-log_custom_message "Published Version" "${FINAL_PUBLISHED_VERSIONS}"
+FINAL_PUBLISHED_VERSION="$(ci-pkginfo -t pkgsemver)"
+log_custom_message "Published Version" "${FINAL_PUBLISHED_VERSION}"
+
+# upload artifact version to eng prod s3 to be used by downstream jobs
+artifact_version="$(ci-pkginfo -t pkgname)@$(ci-pkginfo -t pkgsemver)"
+if upload_job_data global artifact_version ${artifact_version}; then
+  echo "Upload okta-auth-js job data artifact_version=${artifact_version} to s3!"
+else
+  # only echo the info since the upload is not crucial
+  echo "Fail to upload okta-auth-js job data artifact_version=${artifact_version} to s3!" >&2
+fi
 
 popd
 
