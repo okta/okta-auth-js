246e3e2b75e5ace57800c7380a3e815dfd33e4d0
Aaron Granick <aaron.granick@okta.com>
2022-04-14 18:59:06 +0000

    removes support for legacy PKCE / oauth storage
    OKTA-362589
    <<<Jenkins Check-In of Tested SHA: 64c5b977b63c9ac73ed042f19b225144794810ef for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-auth-js
    Files changed count: 17
    PR Link: "https://github.com/okta/okta-auth-js/pull/1181"

--- test/spec/TransactionManager.ts
+++ test/spec/TransactionManager.ts
@@ -42,15 +42,11 @@ describe('TransactionManager', () => {
   beforeEach(() => {
     const transactionStorage = createMockStorage();
     const idxResponseStorage = createMockStorage();
-    const legacyPKCEStorage = createMockStorage();
-    const legacyOAuthParamsStorage = createMockStorage();
     const sharedTransactionStorage = createMockStorage();
     const storageManager = {
       storageUtil: {},
       getTransactionStorage: jest.fn().mockReturnValue(transactionStorage),
       getIdxResponseStorage: jest.fn().mockReturnValue(idxResponseStorage),
-      getLegacyPKCEStorage: jest.fn().mockReturnValue(legacyPKCEStorage),
-      getLegacyOAuthParamsStorage: jest.fn().mockReturnValue(legacyOAuthParamsStorage),
       getSharedTransactionStorage: jest.fn().mockReturnValue(sharedTransactionStorage)
     } as unknown as StorageManager;
     const options = { storageManager };
@@ -71,8 +67,6 @@ describe('TransactionManager', () => {
       storageManager,
       transactionStorage,
       idxResponseStorage,
-      legacyPKCEStorage,
-      legacyOAuthParamsStorage,
       sharedTransactionStorage,
       options,
       meta
@@ -111,18 +105,6 @@ describe('TransactionManager', () => {
       const { transactionManager, idxResponseStorage } = testContext;
       transactionManager.clear({ clearIdxResponse: false });
       expect(idxResponseStorage.clearStorage).not.toHaveBeenCalled();
-    });
-
-    // This is for compatibility with older versions of the signin widget. OKTA-304806
-    it('pkce: clears legacy PKCE meta', () => {
-      const { storageManager, transactionManager, legacyPKCEStorage } = testContext;
-      transactionManager.clear({
-        pkce: true
-      });
-      expect(storageManager.getLegacyPKCEStorage).toHaveBeenCalledTimes(2);
-      expect(legacyPKCEStorage.clearStorage).toHaveBeenCalledTimes(2);
-      expect(storageManager.getLegacyPKCEStorage).toHaveBeenNthCalledWith(1, { storageType: 'localStorage' });
-      expect(storageManager.getLegacyPKCEStorage).toHaveBeenNthCalledWith(2, { storageType: 'sessionStorage' });
     });
 
     describe('shared transaction storage', () => {
@@ -172,165 +154,32 @@ describe('TransactionManager', () => {
       expect(transactionStorage.setStorage).toHaveBeenCalledWith(meta);
     });
 
-    describe('oauth', () => {
+    describe('shared transaction storage', () => {
       beforeEach(() => {
-        const { storageManager } = testContext;
-        const cookieStorage = {
-          setItem: jest.fn()
-        };
-        storageManager.getStorage = jest.fn().mockImplementation((options) => {
-          if (options.storageType !== 'cookie') {
-            throw new Error(`unexpected storage type ${options.storageType}`);
-          }
-          return cookieStorage;
-        });
-        const sharedStorageObject = {};
-        const sharedStorage = {
-          setStorage: jest.fn(),
-          getStorage: jest.fn().mockReturnValue(sharedStorageObject)
-        };
-        storageManager.getSharedTansactionStorage = jest.fn().mockReturnValue(sharedStorage);
-        Object.assign(testContext, {
-          cookieStorage,
-          sharedStorage,
-          sharedStorageObject
-        });
+        jest.spyOn(mocked.sharedStorage, 'saveTransactionToSharedStorage');
       });
-
-      it('by default, saves 3 cookies: params, nonce, state', () => {
+      it('saves to shared transaction storage by default', () => {
         createInstance();
-        const { transactionManager, meta, cookieStorage } = testContext;
-        transactionManager.save(meta, { oauth: true });
-        expect(cookieStorage.setItem).toHaveBeenCalledTimes(3);
-        expect(cookieStorage.setItem).toHaveBeenNthCalledWith(1,
-          'okta-oauth-redirect-params',
-          '{"responseType":"code","state":"mock-state","nonce":"mock-nonce","scopes":["a","b"],"clientId":"mock-clientid","urls":{"a":"b"},"ignoreSignature":true}',
-          null
-        );
-        expect(cookieStorage.setItem).toHaveBeenNthCalledWith(2,
-          'okta-oauth-nonce',
-          'mock-nonce',
-          null
-        );
-        expect(cookieStorage.setItem).toHaveBeenNthCalledWith(3,
-          'okta-oauth-state',
-          'mock-state',
-          null
-        );
+        const { transactionManager, meta, storageManager } = testContext;
+        transactionManager.save(meta);
+        expect(mocked.sharedStorage.saveTransactionToSharedStorage).toHaveBeenCalledWith(storageManager, meta.state, meta);
       });
-
-      describe('saveParamsCookie', () => {
-        beforeEach(() => {
-          const { options } = testContext;
-          options.saveNonceCookie = false;
-          options.saveStateCookie = false;
-        });
-        it('is enabled by default', () => {
-          createInstance();
-          const { transactionManager, meta, cookieStorage } = testContext;
-          transactionManager.save(meta, { oauth: true });
-          expect(cookieStorage.setItem).toHaveBeenCalledWith(
-            'okta-oauth-redirect-params',
-            '{"responseType":"code","state":"mock-state","nonce":"mock-nonce","scopes":["a","b"],"clientId":"mock-clientid","urls":{"a":"b"},"ignoreSignature":true}',
-            null
-          );
-        });
-        it('can be disabled via option', () => {
-          createInstance({ saveParamsCookie: false });
-          const { transactionManager, meta, cookieStorage } = testContext;
-          transactionManager.save(meta, { oauth: true });
-          expect(cookieStorage.setItem).not.toHaveBeenCalled();
-        });
+      it('can be disabled via option', () => {
+        createInstance({ enableSharedStorage: false });
+        const { transactionManager, meta } = testContext;
+        transactionManager.save(meta);
+        expect(mocked.sharedStorage.saveTransactionToSharedStorage).not.toHaveBeenCalled();
       });
-
-      describe('saveNonceCookie', () => {
-        beforeEach(() => {
-          const { options } = testContext;
-          options.saveParamsCookie = false;
-          options.saveStateCookie = false;
-        });
-        it('is enabled by default', () => {
-          createInstance();
-          const { transactionManager, meta, cookieStorage } = testContext;
-          transactionManager.save(meta, { oauth: true });
-          expect(cookieStorage.setItem).toHaveBeenCalledWith(
-            'okta-oauth-nonce',
-            'mock-nonce',
-            null
-          );
-        });
-        it('can be disabled via option', () => {
-          createInstance({ saveNonceCookie: false });
-          const { transactionManager, meta, cookieStorage } = testContext;
-          transactionManager.save(meta, { oauth: true });
-          expect(cookieStorage.setItem).not.toHaveBeenCalled();
-        });
-        it('will not set a cookie if there is no nonce', () => {
-          createInstance();
-          const { transactionManager, meta, cookieStorage } = testContext;
-          delete meta.nonce;
-          transactionManager.save(meta, { oauth: true });
-          expect(cookieStorage.setItem).not.toHaveBeenCalled();
-        });
-      });
-
-      describe('saveStateCookie', () => {
-        beforeEach(() => {
-          const { options } = testContext;
-          options.saveParamsCookie = false;
-          options.saveNonceCookie = false;
-        });
-        it('is enabled by default', () => {
-          createInstance();
-          const { transactionManager, meta, cookieStorage } = testContext;
-          transactionManager.save(meta, { oauth: true });
-          expect(cookieStorage.setItem).toHaveBeenCalledWith(
-            'okta-oauth-state',
-            'mock-state',
-            null
-          );
-        });
-        it('can be disabled via option', () => {
-          createInstance({ saveStateCookie: false });
-          const { transactionManager, meta, cookieStorage } = testContext;
-          transactionManager.save(meta, { oauth: true });
-          expect(cookieStorage.setItem).not.toHaveBeenCalled();
-        });
-        it('will not set a cookie if there is no state', () => {
-          createInstance();
-          const { transactionManager, meta, cookieStorage } = testContext;
-          delete meta.state;
-          transactionManager.save(meta, { oauth: true });
-          expect(cookieStorage.setItem).not.toHaveBeenCalled();
-        });
-      });
-
-      describe('shared transaction storage', () => {
-        beforeEach(() => {
-          jest.spyOn(mocked.sharedStorage, 'saveTransactionToSharedStorage');
-        });
-        it('saves to shared transaction storage by default', () => {
-          createInstance();
-          const { transactionManager, meta, storageManager } = testContext;
-          transactionManager.save(meta, { oauth: true });
-          expect(mocked.sharedStorage.saveTransactionToSharedStorage).toHaveBeenCalledWith(storageManager, meta.state, meta);
-        });
-        it('can be disabled via option', () => {
-          createInstance({ enableSharedStorage: false });
-          const { transactionManager, meta } = testContext;
-          transactionManager.save(meta, { oauth: true });
-          expect(mocked.sharedStorage.saveTransactionToSharedStorage).not.toHaveBeenCalled();
-        });
-        it('will not save if there is no state', () => {
-          createInstance();
-          const { transactionManager, meta } = testContext;
-          delete meta.state;
-          transactionManager.save(meta, { oauth: true });
-          expect(mocked.sharedStorage.saveTransactionToSharedStorage).not.toHaveBeenCalled();
-        });
+      it('will not save if there is no state', () => {
+        createInstance();
+        const { transactionManager, meta } = testContext;
+        delete meta.state;
+        transactionManager.save(meta);
+        expect(mocked.sharedStorage.saveTransactionToSharedStorage).not.toHaveBeenCalled();
       });
     });
   });
+  
 
   describe('load', () => {
     beforeEach(() => {
@@ -409,226 +258,6 @@ describe('TransactionManager', () => {
       });
     });
 
-    describe('pkce', () => {
-      it('pkce: true throws an error if meta cannot be found', () => {
-        createInstance();
-        const { transactionManager } = testContext;
-        const fn = () => {
-          transactionManager.load({
-            pkce: true
-          });
-        };
-        expect(fn).toThrowError('Could not load PKCE codeVerifier from storage');
-      });
-      it('pkce: false does not throw', () => {
-        createInstance();
-        const { transactionManager } = testContext;
-        const fn = () => {
-          transactionManager.load({
-            pkce: false
-          });
-        };
-        expect(fn).not.toThrow();
-      });
-      it('calls clearLegacyPKCE', () => {
-        createInstance();
-        const { legacyPKCEStorage, transactionStorage, transactionManager, meta } = testContext;
-        jest.spyOn(transactionManager, 'clearLegacyPKCE').mockReturnValue(null);
-        jest.spyOn(transactionManager, 'loadLegacyPKCE').mockReturnValue({
-          codeVerifier: 'abc'
-        });
-        transactionStorage.getStorage.mockReturnValue({}); // nothing in transaction storage
-        legacyPKCEStorage.getStorage.mockReturnValue(meta);
-        transactionManager.load({
-          pkce: true
-        });
-        expect(transactionManager.clearLegacyPKCE).toHaveBeenCalledWith();
-      });
-    });
-
-    describe('oauth', () => {
-      it('oauth: true throws an error if params cannot be found', () => {
-        createInstance();
-        const { transactionManager } = testContext;
-        jest.spyOn(transactionManager, 'loadLegacyOAuthParams');
-        const fn = () => {
-          transactionManager.load({
-            oauth: true
-          });
-        };
-        expect(fn).toThrowError('Unable to retrieve OAuth redirect params from storage');
-        expect(transactionManager.loadLegacyOAuthParams).toHaveBeenCalledWith();
-      });
-      it('oauth: false does not throw', () => {
-        createInstance();
-        const { transactionManager } = testContext;
-        jest.spyOn(transactionManager, 'loadLegacyOAuthParams');
-        const fn = () => {
-          transactionManager.load({
-            oauth: false
-          });
-        };
-        expect(fn).not.toThrow();
-        expect(transactionManager.loadLegacyOAuthParams).not.toHaveBeenCalledWith();
-      });
-      it('calls clearLegacyOAuthParams', () => {
-        createInstance();
-        const { transactionStorage, transactionManager } = testContext;
-        jest.spyOn(transactionManager, 'clearLegacyOAuthParams').mockReturnValue(null);
-        jest.spyOn(transactionManager, 'loadLegacyOAuthParams').mockReturnValue({
-          redirectUri: 'http://fake'
-        });
-        transactionStorage.getStorage.mockReturnValue({}); // no transaction data
-        transactionManager.load({
-          oauth: true
-        });
-        expect(transactionManager.clearLegacyOAuthParams).toHaveBeenCalledWith();
-      });
-    });
-
-    describe('pkce + oauth', () => {
-      it('throws an error if OAuth params cannot be found', () => {
-        createInstance();
-        const { transactionManager } = testContext;
-        const fn = () => {
-          transactionManager.load({
-            pkce: true,
-            oauth: true
-          });
-        };
-        expect(fn).toThrowError('Unable to retrieve OAuth redirect params from storage');
-      });
-    });
-
-     // This is for compatibility with older versions of the signin widget. OKTA-304806
-    describe('if no transaction data, try to load from legacy PKCE meta', () => {
-      beforeEach(() => {
-        const { transactionStorage } = testContext;
-        transactionStorage.getStorage.mockReturnValue({}); // no transaction data
-      });
-
-      it('try localStorage first', () => {
-        createInstance();
-        const { storageManager, transactionManager, meta } = testContext;
-        const getStorage = jest.fn().mockReturnValue(meta);
-        jest.spyOn(storageManager, 'getLegacyPKCEStorage').mockReturnValue({
-          getStorage,
-          clearStorage: jest.fn()
-        });
-        const res = transactionManager.load({
-          pkce: true
-        });
-        expect(res).toEqual(meta);
-        expect(storageManager.getLegacyPKCEStorage).toHaveBeenCalledWith({ storageType: 'localStorage' });
-        expect(getStorage).toHaveBeenCalledTimes(1);
-      });
-
-      it('check sessionStorage if localStorage has no data', () => {
-        createInstance();
-        const { legacyPKCEStorage, storageManager, transactionManager, meta } = testContext;
-        legacyPKCEStorage.getStorage.mockReturnValueOnce({});
-        legacyPKCEStorage.getStorage.mockReturnValueOnce(meta);
-        const res = transactionManager.load({
-          pkce: true
-        });
-        expect(res).toEqual(meta);
-        expect(storageManager.getLegacyPKCEStorage).toHaveBeenNthCalledWith(1, { storageType: 'localStorage' });
-        expect(storageManager.getLegacyPKCEStorage).toHaveBeenNthCalledWith(2, { storageType: 'sessionStorage' });
-        expect(legacyPKCEStorage.getStorage).toHaveBeenCalledTimes(2);
-      });
-    });
-  });
-
-  describe('loadLegacyPKCE', () => {
-    beforeEach(() => {
-      createInstance();
-      const { storageManager } = testContext;
-      Object.assign(storageManager.storageUtil, {
-        testStorageType: jest.fn().mockReturnValue(true)
-      });
-    });
-
-    it('try localStorage first', () => {
-      const { storageManager, transactionManager, legacyPKCEStorage, meta } = testContext;
-      legacyPKCEStorage.getStorage.mockReturnValue(meta);
-      const res = transactionManager.loadLegacyPKCE();
-      expect(res).toEqual(meta);
-      expect(storageManager.getLegacyPKCEStorage).toHaveBeenCalledTimes(1);
-      expect(storageManager.getLegacyPKCEStorage).toHaveBeenNthCalledWith(1, { storageType: 'localStorage' });
-    });
-
-    it('check sessionStorage if localStorage has no data', () => {
-      const { legacyPKCEStorage, storageManager, transactionManager, meta } = testContext;
-      const getStorage = legacyPKCEStorage.getStorage;
-      getStorage.mockReturnValueOnce({});
-      getStorage.mockReturnValueOnce(meta);
-      const res = transactionManager.loadLegacyPKCE();
-      expect(res).toEqual(meta);
-      expect(storageManager.getLegacyPKCEStorage).toHaveBeenNthCalledWith(1, { storageType: 'localStorage' });
-      expect(storageManager.getLegacyPKCEStorage).toHaveBeenNthCalledWith(2, { storageType: 'sessionStorage' });
-      expect(getStorage).toHaveBeenCalledTimes(2);
-    });
-  });
-
-  describe('loadLegacyOAuthParams', () => {
-    beforeEach(() => {
-      const fakeParams = { fake: 'fake', redirectUri: 'http://alsofake' };
-      createInstance();
-      const { storageManager } = testContext;
-      Object.assign(storageManager.storageUtil, {
-        testStorageType: jest.fn()
-      });
-      Object.assign(testContext, {
-        fakeParams
-      });
-    });
-
-    describe('has sessionStorage', () => {
-      it('should read from sessionStorage', () => {
-        const { storageManager, legacyOAuthParamsStorage, fakeParams, transactionManager } = testContext;
-        storageManager.storageUtil.testStorageType.mockReturnValueOnce(true);
-        legacyOAuthParamsStorage.getStorage.mockReturnValue(fakeParams);
-        const res = transactionManager.loadLegacyOAuthParams();
-        expect(res).toEqual(fakeParams);
-        expect(storageManager.getLegacyOAuthParamsStorage).toHaveBeenCalledTimes(1);
-        expect(storageManager.getLegacyOAuthParamsStorage).toHaveBeenCalledWith({ storageType: 'sessionStorage' });
-      });
-
-      it('should read from cookies when no data in sessionStorage', () => {
-        const { storageManager, legacyOAuthParamsStorage, fakeParams, transactionManager } = testContext;
-        storageManager.storageUtil.testStorageType.mockReturnValue(true);
-        legacyOAuthParamsStorage.getStorage.mockReturnValueOnce({});
-        legacyOAuthParamsStorage.getStorage.mockReturnValueOnce(fakeParams);
-        const res = transactionManager.loadLegacyOAuthParams();
-        expect(res).toEqual(fakeParams);
-        expect(storageManager.getLegacyOAuthParamsStorage).toHaveBeenCalledTimes(2);
-        expect(storageManager.getLegacyOAuthParamsStorage).toHaveBeenNthCalledWith(1, { storageType: 'sessionStorage' });
-        expect(storageManager.getLegacyOAuthParamsStorage).toHaveBeenNthCalledWith(2, { storageType: 'cookie' });
-    });
-    });
-
-    describe('not has sessionStorage', () => {
-      it('should read from cookies', () => {
-        const { storageManager, legacyOAuthParamsStorage, fakeParams, transactionManager } = testContext;
-        storageManager.storageUtil.testStorageType.mockReturnValueOnce(false);
-        storageManager.storageUtil.testStorageType.mockReturnValueOnce(true);
-        legacyOAuthParamsStorage.getStorage.mockReturnValue(fakeParams);
-        const res = transactionManager.loadLegacyOAuthParams();
-        expect(res).toEqual(fakeParams);
-        expect(storageManager.getLegacyOAuthParamsStorage).toHaveBeenCalledTimes(1);
-        expect(storageManager.getLegacyOAuthParamsStorage).toHaveBeenNthCalledWith(1, { storageType: 'cookie' });
-      });
-    });
-
-    describe('no data in session or cookie', () => {
-      it('throws', () => {
-        const { transactionManager } = testContext;
-        const fn = () => {
-          transactionManager.loadLegacyOAuthParams();
-        };
-        expect(fn).toThrowError('Unable to retrieve OAuth redirect params from storage');
-      });
-    });
   });
 
   describe('saveIdxResponse', () => {
