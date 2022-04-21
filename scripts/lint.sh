84128aadfcf11dac22a70974a9ab1cd0a53850ca
Aaron Granick <aaron.granick@okta.com>
2022-04-20 23:14:55 -0700

    Update lint.sh

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -4,6 +4,10 @@ source ${OKTA_HOME}/${REPO}/scripts/setup.sh
 
 export TEST_SUITE_TYPE="checkstyle"
 export TEST_RESULT_FILE_DIR="${REPO}/build2"
+echo $TEST_SUITE_TYPE > $TEST_SUITE_TYPE_FILE
+echo $TEST_RESULT_FILE_DIR > $TEST_RESULT_FILE_DIR_FILE
+
+mkdir -p ${TEST_RESULT_FILE_DIR}
 
 if ! yarn tsd; then
   echo "tsd failed! Exiting..."
