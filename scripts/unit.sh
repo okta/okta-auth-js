eabe4214442ab63b2e8e7f0a3e7f9eed3fb001cf
Aaron Granick <aaron.granick@okta.com>
2022-04-29 13:48:41 -0700

    Update unit.sh

--- scripts/unit.sh
+++ scripts/unit.sh
@@ -7,22 +7,22 @@ export TEST_RESULT_FILE_DIR="${REPO}/build2/reports/unit"
 
 if ! yarn test:unit; then
   echo "unit failed! Exiting..."
-  exit ${TEST_FAILURE}
+  exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_ALWAYS_FAIL}
 fi
 
 if ! yarn test:bundle:esm:browser; then
   echo "validate ESM browser bundle failed! Exiting..."
-  exit ${TEST_FAILURE}
+  exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_ALWAYS_FAIL}
 fi
 
 if ! yarn test:bundle:esm:node; then
   echo "validate ESM node bundle failed! Exiting..."
-  exit ${TEST_FAILURE}
+  exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_ALWAYS_FAIL}
 fi
 
 if ! yarn test:bundle:cjs; then
   echo "validate cjs bundle failed! Exiting..."
-  exit ${TEST_FAILURE}
+  exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_ALWAYS_FAIL}
 fi
 
 echo ${TEST_SUITE_TYPE} > ${TEST_SUITE_TYPE_FILE}
