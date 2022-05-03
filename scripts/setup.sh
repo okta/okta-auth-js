3c20808b3ec3654d5f37b1482e6dda139270c3b7
eng-prod-CI-bot-okta <aaron.granick@okta.com>
2022-05-03 17:38:54 +0000

    Downstream Artifact Test for commit 4716b880fd03fb19f101e633f622b938478afbc8 in artifact okta-signin-widget

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,7 +3,7 @@
 # Can be used to run a canary build against a beta AuthJS version that has been published to artifactory.
 # This is available from the "downstream artifact" menu on any okta-auth-js build in Bacon.
 # DO NOT MERGE ANY CHANGES TO THIS LINE!!
-export WIDGET_VERSION=""
+export WIDGET_VERSION="6.4.0-g4716b88"
 
 # Add yarn to the $PATH so npm cli commands do not fail
 export PATH="${PATH}:$(yarn global bin)"
