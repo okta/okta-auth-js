2f21ab2e4caef71f45b2a37e705203b7db59a1c8
Aaron Granick <aaron.granick@okta.com>
2022-05-09 16:07:06 +0000

    breaking change: do not throw IDX responses as errors
    OKTA-481844
    <<<Jenkins Check-In of Tested SHA: c2ae02682159a3bd2117e8289e8f5233a17c34fb for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-auth-js
    Files changed count: 15
    PR Link: https://github.com/okta/okta-auth-js/pull/1205

--- scripts/unit.sh
+++ scripts/unit.sh
@@ -4,10 +4,12 @@ source ${OKTA_HOME}/${REPO}/scripts/setup.sh "v14.18.0"
 
 export TEST_SUITE_TYPE="junit"
 export TEST_RESULT_FILE_DIR="${REPO}/build2/reports/unit"
+echo ${TEST_SUITE_TYPE} > ${TEST_SUITE_TYPE_FILE}
+echo ${TEST_RESULT_FILE_DIR} > ${TEST_RESULT_FILE_DIR_FILE}
 
 if ! yarn test:unit; then
   echo "unit failed! Exiting..."
-  exit ${TEST_FAILURE}
+  exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_ALWAYS_FAIL}
 fi
 
 if ! yarn test:types; then
@@ -17,19 +19,17 @@ fi
 
 if ! yarn test:bundle:esm:browser; then
   echo "validate ESM browser bundle failed! Exiting..."
-  exit ${TEST_FAILURE}
+  exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_ALWAYS_FAIL}
 fi
 
 if ! yarn test:bundle:esm:node; then
   echo "validate ESM node bundle failed! Exiting..."
-  exit ${TEST_FAILURE}
+  exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_ALWAYS_FAIL}
 fi
 
 if ! yarn test:bundle:cjs; then
   echo "validate cjs bundle failed! Exiting..."
-  exit ${TEST_FAILURE}
+  exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_ALWAYS_FAIL}
 fi
 
-echo ${TEST_SUITE_TYPE} > ${TEST_SUITE_TYPE_FILE}
-echo ${TEST_RESULT_FILE_DIR} > ${TEST_RESULT_FILE_DIR_FILE}
 exit ${PUBLISH_TYPE_AND_RESULT_DIR}
