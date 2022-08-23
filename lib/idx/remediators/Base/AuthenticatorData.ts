0b0d05a6318a8d3657113a59dbc6958a01785324
Shuo Wu <wushuo2010@gmail.com>
2022-08-23 14:08:22 -0400

    dummy commit

--- lib/idx/remediators/Base/AuthenticatorData.ts
+++ lib/idx/remediators/Base/AuthenticatorData.ts
@@ -11,7 +11,6 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-
 import { Remediator, RemediationValues } from './Remediator';
 import { IdxRemediationValue, IdxOption, IdxRemediation, IdxAuthenticator } from '../../types/idx-js';
 import { isAuthenticator } from '../../types';
