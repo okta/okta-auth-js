c729df6ee547ef290798c01cf02e84252b26af8a
Shuo Wu <wushuo2010@gmail.com>
2022-08-26 13:03:34 -0400

    lint

--- test/apps/verify-entries/src/default.ts
+++ test/apps/verify-entries/src/default.ts
@@ -1,6 +1,7 @@
 import { OktaAuth } from '@okta/okta-auth-js';
 
-new OktaAuth({
+const authClient = new OktaAuth({
   issuer: process.env.ISSUER,
   clientId: process.env.SPA_CLIENT_ID,
 });
+console.log(authClient);
