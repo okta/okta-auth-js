bacf36898592d95f2ad7be8e2a89d506523965a4
Shuo Wu <wushuo2010@gmail.com>
2022-09-28 16:25:49 -0400

    set node version to 14

--- scripts/verify-registry-install.sh
+++ scripts/verify-registry-install.sh
@@ -7,6 +7,8 @@ export REGISTRY="https://artifacts.aue1d.saasure.com/artifactory/npm-topic"
 
 cd ${OKTA_HOME}/${REPO}
 
+NODE_VERSION="${1:-v14.18.0}"
+setup_service node $NODE_VERSION
 # Use the cacert bundled with centos as okta root CA is self-signed and cause issues downloading from yarn
 setup_service yarn 1.21.1 /etc/pki/tls/certs/ca-bundle.crt
 
