40a40bcbf5a0a7e481f54097ac4e295671bb70a0
Shuo Wu <wushuo2010@gmail.com>
2022-09-29 10:44:13 -0400

    set cafile for yarn3

--- scripts/verify-registry-install.sh
+++ scripts/verify-registry-install.sh
@@ -58,6 +58,7 @@ mkdir yarn-v3-test
 pushd yarn-v3-test
 # use yarn v3
 yarn set version stable
+yarn config set cafile /etc/pki/tls/certs/ca-bundle.crt
 yarn init -y
 # add empty lock file, so this dir can be a isolated project
 touch yarn.lock
