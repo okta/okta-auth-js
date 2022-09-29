e408d2ed243dcb41456f66f2b3b319002c338b11
Shuo Wu <wushuo2010@gmail.com>
2022-09-29 11:17:55 -0400

    add pretask back

--- scripts/verify-registry-install.sh
+++ scripts/verify-registry-install.sh
@@ -16,41 +16,41 @@ setup_service yarn 1.22.19 /etc/pki/tls/certs/ca-bundle.crt
 yarn global add @okta/ci-append-sha
 yarn global add @okta/ci-pkginfo
 
-# export PATH="${PATH}:$(yarn global bin)"
+export PATH="${PATH}:$(yarn global bin)"
 
-# # Append a SHA to the version in package.json 
-# if ! ci-append-sha; then
-#   echo "ci-append-sha failed! Exiting..."
-#   exit $FAILED_SETUP
-# fi
+# Append a SHA to the version in package.json 
+if ! ci-append-sha; then
+  echo "ci-append-sha failed! Exiting..."
+  exit $FAILED_SETUP
+fi
 
-# # NOTE: hyphen rather than '@'
-# artifact_version="$(ci-pkginfo -t pkgname)-$(ci-pkginfo -t pkgsemver)"
-# published_tarball=${REGISTRY}/@okta/okta-auth-js/-/${artifact_version}.tgz
+# NOTE: hyphen rather than '@'
+artifact_version="$(ci-pkginfo -t pkgname)-$(ci-pkginfo -t pkgsemver)"
+published_tarball=${REGISTRY}/@okta/okta-auth-js/-/${artifact_version}.tgz
 
-# # verify npm install
-# mkdir npm-test
-# pushd npm-test
-# npm init -y
+# verify npm install
+mkdir npm-test
+pushd npm-test
+npm init -y
 
-# if ! npm i ${published_tarball}; then
-#   echo "npm install ${published_tarball} failed! Exiting..."
-#   exit ${FAILED_SETUP}
-# fi
-# echo "Done with npm installation test"
-# popd
+if ! npm i ${published_tarball}; then
+  echo "npm install ${published_tarball} failed! Exiting..."
+  exit ${FAILED_SETUP}
+fi
+echo "Done with npm installation test"
+popd
 
-# # verify yarn classic install
-# mkdir yarn-classic-test
-# pushd yarn-classic-test
-# yarn init -y
+# verify yarn classic install
+mkdir yarn-classic-test
+pushd yarn-classic-test
+yarn init -y
 
-# if ! yarn add ${published_tarball}; then
-#   echo "yarn-classic install ${published_tarball} failed! Exiting..."
-#   exit ${FAILED_SETUP}
-# fi
-# echo "Done with yarn classic installation test"
-# popd
+if ! yarn add ${published_tarball}; then
+  echo "yarn-classic install ${published_tarball} failed! Exiting..."
+  exit ${FAILED_SETUP}
+fi
+echo "Done with yarn classic installation test"
+popd
 
 # verify yarn v3 install
 mkdir yarn-v3-test
@@ -63,8 +63,7 @@ yarn init -y
 # add empty lock file, so this dir can be a isolated project
 touch yarn.lock
 
-# if ! yarn add @okta/okta-auth-js@${published_tarball}; then
-if ! yarn add @okta/okta-auth-js; then
+if ! yarn add @okta/okta-auth-js@${published_tarball}; then
   echo "yarn-v3 install @okta/okta-auth-js@${published_tarball} failed! Exiting..."
   exit ${FAILED_SETUP}
 fi
