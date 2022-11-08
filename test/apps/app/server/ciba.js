f8155f83e59e58fdba14d7528178be5c6e583862
Shuo Wu <wushuo2010@gmail.com>
2022-11-08 11:25:25 -0500

    update test app to support clientSecret cib auth

--- test/apps/app/server/ciba.js
+++ test/apps/app/server/ciba.js
@@ -7,14 +7,16 @@ async function cibaClientAuthMiddleware(req, res) {
   console.log('cibaClientAuthMiddleware received form data:', req.body);
   const config = JSON.parse(req.body.config);
   const loginHint = req.body.login_hint;
-  const { issuer, clientId, redirectUri } = config;
+  const { issuer, clientId, redirectUri, clientSecret } = config;
 
   const authClient = new OktaAuth({
     issuer,
     clientId,
     scopes: config.scopes,
     redirectUri,
+    // client can be authenticated by either clientSecret or privateKey
     privateKey,
+    clientSecret,
   });
   const { 
     headers, // eslint-disable-line
