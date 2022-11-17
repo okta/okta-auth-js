52ef2e3c8d06929ba662b470eac18705af7e0564
denys.oblohin <denys.oblohin@okta.com>
2022-11-18 01:54:06 +0200

    up

--- scripts/samples/e2e-react-embedded-auth-with-sdk-trex.sh
+++ scripts/samples/e2e-react-embedded-auth-with-sdk-trex.sh
@@ -2,10 +2,10 @@
 
 source $(dirname "${BASH_SOURCE[0]}")/../setup-e2e.sh
 
+export BACON_TASK="true" # to run tests against trexcloud org
 setup_sample_tests
 
 export SAMPLE_NAME=@okta/samples.react-embedded-auth-with-sdk
 export TREX=true
-export BACON_TASK="true"
 
 run_sample_tests
