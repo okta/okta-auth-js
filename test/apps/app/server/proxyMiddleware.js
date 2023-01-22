8e6d28f43ceb84c82aa0dd54b103ab661d1188ca
denys.oblohin <denys.oblohin@okta.com>
2023-01-23 01:09:19 +0200

    Revert "try set-cookies"
    This reverts commit 57ebb4be8dbb172c34f2dc0e48d2e623ef3b7524.

--- test/apps/app/server/proxyMiddleware.js
+++ test/apps/app/server/proxyMiddleware.js
@@ -17,54 +17,21 @@ module.exports = function proxyMiddlewareFactory({ proxyPort, origin }) {
     target: origin,
     secure: false,
     changeOrigin: true,
-   // cookieDomainRewrite: 'localhost',
-   // selfHandleResponse: true, 
-    onProxyRes: (proxyRes, req, res) => {
-      if (req.url.includes('/oauth2/v1/authorize')) {
-        console.log('-------------')
-        console.log('-------------')
-        console.log('-------------')
-        const cs = proxyRes.headers['set-cookie'];
-        const cs2 = [];
-        for (const c of cs) {
-          let c2 = c;
-          c2 = c2.replace('; Secure', '');
-          c2 = c2 + '; Domain=javascript-idx-sdk.okta.com';
-          cs2.push(c2);
-        }
-        console.log(cs2)
-        proxyRes.headers['set-cookie'] = cs2;
+    selfHandleResponse: true, 
+    onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
+      const response = responseBuffer.toString('utf8');
+      let patchedResponse = response;
+      patchedResponse = patchedResponse.replace(
+        buildRegexForUri(origin),
+        escapeUri(`http://localhost:${proxyPort}`)
+      );
+      if (!req.url.includes('/.well-known') ) {
+        patchedResponse = patchedResponse.replace(
+          new RegExp(origin, 'g'),
+          `http://localhost:${proxyPort}`
+        );
       }
-    },
-    // onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
-    //   const response = responseBuffer.toString('utf8');
-    //   let patchedResponse = response;
-    //   // patchedResponse = patchedResponse.replace(
-    //   //   buildRegexForUri(origin),
-    //   //   escapeUri(`http://localhost:${proxyPort}`)
-    //   // );
-    //   // if (!req.url.includes('/.well-known') ) {
-    //   //   patchedResponse = patchedResponse.replace(
-    //   //     new RegExp(origin, 'g'),
-    //   //     `http://localhost:${proxyPort}`
-    //   //   );
-    //   // }
-    //   if (req.url.includes('/oauth2/v1/authorize')) {
-    //     console.log('-------------')
-    //     console.log('-------------')
-    //     console.log('-------------')
-    //     const cs = proxyRes.headers['set-cookie'];
-    //     const cs2 = [];
-    //     for (const c of cs) {
-    //       let c2 = c;
-    //       c2 = c2.replace('; Secure', '');
-    //       c2 = c2 + '; Domain=javascript-idx-sdk.okta.com';
-    //       cs2.push(c2);
-    //     }
-    //     console.log(cs2)
-    //     proxyRes.headers['set-cookie'] = cs2;
-    //   }
-    //   return patchedResponse;
-    // }),
+      return patchedResponse;
+    }),
   });
 };
