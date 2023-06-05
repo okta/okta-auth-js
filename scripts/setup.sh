3d0d1b41e2faa360a3ef370abd3798956b446335
Shaw Yu <shaw.yu@okta.com>
2023-06-05 16:50:45 -0700

    Revert "use yarn-berry"
    This reverts commit d71a4518e7b8bc30c91f8425284a1c4a7aae3077.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -16,7 +16,8 @@ if [ -n "${TEST_SUITE_ID}" ]; then
   export NVM_DIR="/root/.nvm"
 
   setup_service node "${1:-v14.18.0}"
-  setup_service yarn-berry 1.21.1
+  # Use the cacert bundled with centos as okta root CA is self-signed and cause issues downloading from yarn
+  setup_service yarn 1.21.1 /etc/pki/tls/certs/ca-bundle.crt
 
 else
   # bacon defines OKTA_HOME and REPO, define these relative to this file
