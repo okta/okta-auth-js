933fff3fb15fa7348112bc51f3decd6ae48ef914
Denys Oblohin <72614880+denysoblohin-okta@users.noreply.github.com>
2023-06-23 15:32:59 +0300

    Use siw-platform-scripts instead of artifactory url (#1417)
    OKTA-607419 Use siw-platform-scripts instead of artifactory url

---
+++ scripts/utils/siw-platform-scripts.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# if running on bacon
+if [ -n "${TEST_SUITE_ID}" ]; then
+  export SIW_PLATFORM_ENV="bacon"
+else
+  export SIW_PLATFORM_ENV="local"
+fi
+
+orig_ssl=$(yarn config get strict-ssl)
+orig_registry=$(yarn config get @okta:registry)
+REGISTRY="${ARTIFACTORY_URL}/api/npm/npm-topic"
+
+update_yarn_config () {
+  if [ "$SIW_PLATFORM_ENV" == "local" ] ; then
+    yarn config set @okta:registry ${REGISTRY}
+    yarn config set strict-ssl false
+    trap restore_yarn_config EXIT
+  fi
+}
+
+restore_yarn_config () {
+  if [ "$SIW_PLATFORM_ENV" == "local" ] ; then
+    if [ "$orig_registry" == "undefined" ] ; then
+      yarn config delete @okta:registry
+    else
+      yarn config set @okta:registry $orig_registry
+    fi
+    yarn config set strict-ssl $orig_ssl
+  fi
+}
+
+install_siw_platform_scripts () {
+  update_yarn_config
+  if ! yarn global add @okta/siw-platform-scripts ; then
+    echo "siw-platform-scripts could not be installed"
+    exit ${FAILED_SETUP}
+  fi
+  restore_yarn_config
+}
+
+install_artifact () {
+  # $1 = package name
+  # $2 = version
+  update_yarn_config
+  if ! siw-platform install-artifact -e ${SIW_PLATFORM_ENV} -n $1 -v $2 ; then
+    echo "$1 $2 could not be installed via siw-platform"
+    exit ${FAILED_SETUP}
+  fi
+  restore_yarn_config
+}
