bf6aa0476a674231824c8d841ee82300c3cc8da8
denys.oblohin <denys.oblohin@okta.com>
2023-07-10 16:36:58 +0300

    add test

--- test/spec/idx/remediators/SelectAuthenticator.ts
+++ test/spec/idx/remediators/SelectAuthenticator.ts
@@ -1,21 +1,24 @@
-import { SelectAuthenticatorAuthenticate } from '../../../../lib/idx/remediators';
+import { SelectAuthenticatorAuthenticate, SelectAuthenticatorEnroll } from '../../../../lib/idx/remediators';
 import {
   SelectAuthenticatorEnrollRemediationFactory,
+  SelectAuthenticatorAuthenticateRemediationFactory,
   AuthenticatorValueFactory,
   PhoneAuthenticatorOptionFactory,
+  EmailAuthenticatorOptionFactory,
   IdxContextFactory,
   PhoneAuthenticatorFactory,
+  EmailAuthenticatorFactory,
 } from '@okta/test.support/idx';
 
-describe('remediators/Base/SelectAuthenticator', () => {
+describe('remediators/SelectAuthenticatorEnroll', () => {
   describe('canRemediate', () => {
     it('retuns false if matched authenticator is already the current one', () => {
-      const currentAuthenticator = PhoneAuthenticatorFactory.build();
+      const currentAuthenticator = EmailAuthenticatorFactory.build();
       const remediation = SelectAuthenticatorEnrollRemediationFactory.build({
         value: [
           AuthenticatorValueFactory.build({
             options: [
-              PhoneAuthenticatorOptionFactory.params({
+              EmailAuthenticatorOptionFactory.params({
                 _authenticator: currentAuthenticator,
               }).build(),
             ]
@@ -29,6 +32,36 @@ describe('remediators/Base/SelectAuthenticator', () => {
       });
       const authenticators = [
         currentAuthenticator,
+      ];
+      const r = new SelectAuthenticatorEnroll(remediation, { authenticators });
+      expect(r.canRemediate(context)).toBe(false);
+      expect(r.canRemediate()).toBe(true);
+    });
+  });
+});
+
+describe('remediators/SelectAuthenticatorAuthenticate', () => {
+  describe('canRemediate', () => {
+    it('retuns false if matched authenticator is already the current one', () => {
+      const currentAuthenticatorEnrollment = PhoneAuthenticatorFactory.build();
+      const remediation = SelectAuthenticatorAuthenticateRemediationFactory.build({
+        value: [
+          AuthenticatorValueFactory.build({
+            options: [
+              PhoneAuthenticatorOptionFactory.params({
+                _authenticator: currentAuthenticatorEnrollment,
+              }).build(),
+            ]
+          }),
+        ]
+      });
+      const context = IdxContextFactory.build({
+        currentAuthenticatorEnrollment: {
+          value: currentAuthenticatorEnrollment
+        }
+      });
+      const authenticators = [
+        currentAuthenticatorEnrollment,
       ];
       const r = new SelectAuthenticatorAuthenticate(remediation, { authenticators });
       expect(r.canRemediate(context)).toBe(false);
