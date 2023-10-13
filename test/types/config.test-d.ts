696950b93c9cf585dd8a19deacb1d9c3e39915b3
denys.oblohin <denys.oblohin@okta.com>
2023-10-13 17:06:04 +0300

    test fix

--- test/types/config.test-d.ts
+++ test/types/config.test-d.ts
@@ -99,7 +99,11 @@ const config: OktaAuthOptions = {
     window.location.href = toRelativeUrl(originalUri, window.location.origin);
   },
   httpRequestClient: function(method, url, args) {
-    return Promise.resolve(null);
+    return Promise.resolve({
+      responseText: 'fake',
+      status: 200,
+      headers: []
+    });
   }
 };
 
