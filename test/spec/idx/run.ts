9b448a617192e87b7598b968d55621f0ad9a8dd9
Glen <glen.fannin@okta.com>
2024-04-12 11:38:41 -0400

    OKTA-716396 : fix : marking to save response when received a valid IDX response

--- test/spec/idx/run.ts
+++ test/spec/idx/run.ts
@@ -372,13 +372,27 @@ describe('idx/run', () => {
         idxResponse.requestDidSucceed = false;
       });
   
-      // Do not save the failed response. Use previous saved IDX resposne to continue
-      it('does not save the idxResponse', async () =>{
+      // Do not save the response if it is not a rawIdxResponse. Use previous saved IDX resposne to continue
+      it('does not save the idxResponse when it is not a rawIdxResponse', async () =>{
         const { authClient, idxResponse } = testContext;
         idxResponse.requestDidSucceed = false;
         jest.spyOn(authClient.transactionManager, 'saveIdxResponse');
         await run(authClient);
-        expect(authClient.transactionManager.saveIdxResponse).not.toHaveBeenCalled();
+        expect(authClient.transactionManager.saveIdxResponse).toHaveBeenCalled();
+      });
+
+      // Will save the idxResponse if it is a rawIdxResponse, even if the response failed
+      it('saves idxResponse', async () => {
+        const { authClient, idxResponse, transactionMeta } = testContext;
+        idxResponse.requestDidSucceed = false;
+        jest.spyOn(authClient.transactionManager, 'saveIdxResponse');
+        await run(authClient);
+        expect(authClient.transactionManager.saveIdxResponse).toHaveBeenCalledWith({
+          rawIdxResponse: idxResponse.rawIdxState,
+          requestDidSucceed: false,
+          stateHandle: idxResponse.context.stateHandle,
+          interactionHandle: transactionMeta.interactionHandle
+        });
       });
   
       // an error response does not clear the transaction. options may be valid on previous response
