b9c38a3ab733051c6e991f8499523c8a8c16bcf3
Jared Perreault <jared.perreault@okta.com>
2024-08-09 16:38:55 -0400

    fix

--- test/apps/app/src/config.ts
+++ test/apps/app/src/config.ts
@@ -39,37 +39,15 @@ export function getDefaultConfig(): Config {
   const CLIENT_ID = process.env.CLIENT_ID;
   const CLIENT_SECRET = process.env.CLIENT_SECRET || '';
 
-  // return {
-  //   issuer: ISSUER,
-  //   clientId: CLIENT_ID,
-  //   redirectUri: REDIRECT_URI,
-  //   useInteractionCodeFlow: true,
-  //   responseType: ['token', 'id_token'],
-  //   scopes: ['openid', 'email', 'offline_access'],
-  //   pkce: true,
-  //   dpop: true,
-  //   forceRedirect: false,
-  //   siwVersion: DEFAULT_SIW_VERSION,
-  //   siwAuthClient: false,
-  //   idps: '',
-  //   postLogoutRedirectUri: POST_LOGOUT_REDIRECT_URI,
-  //   clientSecret: CLIENT_SECRET,
-  //   defaultScopes: false,
-  //   cookies: {
-  //     secure: true
-  //   },
-  //   enableSharedStorage: true,
-  //   crossTabsCount: DEFAULT_CROSS_TABS_COUNT
-  // };
   return {
     issuer: ISSUER,
     clientId: CLIENT_ID,
     redirectUri: REDIRECT_URI,
     useInteractionCodeFlow: true,
     responseType: ['token', 'id_token'],
-    scopes: ['openid', 'offline_access'],
+    scopes: ['openid', 'email', 'offline_access'],
     pkce: true,
-    dpop: false,
+    dpop: true,
     forceRedirect: false,
     siwVersion: DEFAULT_SIW_VERSION,
     siwAuthClient: false,
@@ -81,29 +59,7 @@ export function getDefaultConfig(): Config {
       secure: true
     },
     enableSharedStorage: true,
-    crossTabsCount: 0,
-    tokenManager: {
-      "storage": "sessionStorage",
-      "expireEarlySeconds": 30
-      },
-    storageManager: {
-      "token": {
-        "storageType": "sessionStorage",
-        "storageTypes": []
-      },
-      "cache": {
-        "storageType": "sessionStorage",
-        "storageTypes": []
-      },
-      "transaction": {
-        "storageType": "sessionStorage",
-        "storageTypes": []
-      }
-    },
-    services: {
-      autoRemove: false,
-      autoRenew: false
-    }
+    crossTabsCount: DEFAULT_CROSS_TABS_COUNT
   };
 }
 
