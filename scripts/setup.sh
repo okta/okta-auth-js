8a62c412a94c6ef0a94883a0fe6ca398e549366e
Jared Perreault <jared.perreault@okta.com>
2024-10-15 10:25:11 -0400

    fix

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -15,9 +15,10 @@ if [ -n "${TEST_SUITE_ID}" ]; then
   # Install required node version
   export NVM_DIR="/root/.nvm"
 
-  setup_service node "${1:-v16.20.2}"
+  setup_service node "${1:-v18.20.4}" 1> /dev/null
   # Use the cacert bundled with centos as okta root CA is self-signed and cause issues downloading from yarn
-  setup_service yarn 1.21.1 /etc/pki/tls/certs/ca-bundle.crt
+  # setup_service yarn 1.21.1 /etc/pki/tls/certs/ca-bundle.crt
+  setup_service yarn-berry 1.21.1
 else
   # bacon defines OKTA_HOME and REPO, define these relative to this file
   export OKTA_HOME=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd)
@@ -76,13 +77,12 @@ fi
 cd ${OKTA_HOME}/${REPO}
 
 create_log_group "Yarn Install"
-nvm install 18.20.4
 # Install dependencies. --ignore-scripts will prevent chromedriver from attempting to install
 if ! yarn install --frozen-lockfile --ignore-scripts; then
   echo "yarn install failed! Exiting..."
   exit ${FAILED_SETUP}
 fi
-nvm use 16
+nvm install 16.20.2
 finish_log_group $?
 
 npm_siw_install () {
