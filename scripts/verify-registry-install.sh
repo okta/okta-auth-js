1fee17f932f544d14178282dff23ec9d3c9962a9
Jared Perreault <90656038+jaredperreault-okta@users.noreply.github.com>
2025-04-22 14:54:43 -0400

    chore: ci update (#1586)
    OKTA-919027 chore: update ci script

--- scripts/verify-registry-install.sh
+++ scripts/verify-registry-install.sh
@@ -10,7 +10,7 @@ cd ${OKTA_HOME}/${REPO}
 NODE_VERSION="${1:-v16.20.2}"
 setup_service node $NODE_VERSION
 # Use the cacert bundled with centos as okta root CA is self-signed and cause issues downloading from yarn
-setup_service yarn 1.22.19 /etc/pki/tls/certs/ca-bundle.crt
+setup_service yarn 1.22.22 /etc/pki/tls/certs/ca-bundle.crt
 
 # Install required dependencies
 yarn global add @okta/ci-append-sha
@@ -25,8 +25,9 @@ if ! ci-append-sha; then
 fi
 
 # NOTE: hyphen rather than '@'
-artifact_version="$(ci-pkginfo -t pkgname)-$(ci-pkginfo -t pkgsemver)"
-published_tarball=${REGISTRY}/@okta/okta-auth-js/-/${artifact_version}.tgz
+artifact_version="$(ci-pkginfo -t pkgsemver)"
+artifact_name="$(ci-pkginfo -t pkgname)-$(ci-pkginfo -t pkgsemver)"
+published_tarball=${REGISTRY}/@okta/okta-auth-js/-/${artifact_name}.tgz
 
 # verify npm install
 mkdir npm-test
@@ -40,12 +41,21 @@ fi
 echo "Done with npm installation test"
 popd
 
+# installs package locally (yarn 1 struggles with registry authentication requirements)
+mkdir okta-auth-js
+cd okta-auth-js
+wget --header="Authorization: Bearer ${NPM_TOKEN}" ${published_tarball} -O ${artifact_version}.tgz
+ls -al
+cd ..
+
 # verify yarn classic install
 mkdir yarn-classic-test
 pushd yarn-classic-test
 yarn init -y
+cp /root/.npmrc .npmrc
+yarn config list
 
-if ! yarn add ${published_tarball}; then
+if ! yarn add ../okta-auth-js/${artifact_version}.tgz; then
   echo "yarn-classic install ${published_tarball} failed! Exiting..."
   exit ${FAILED_SETUP}
 fi
@@ -59,10 +69,12 @@ pushd yarn-v3-test
 yarn set version stable
 yarn config set caFilePath /etc/pki/tls/certs/ca-bundle.crt
 yarn init -y
+cp /root/.npmrc .npmrc
+yarn config list
 # add empty lock file, so this dir can be a isolated project
 touch yarn.lock
 
-if ! yarn add @okta/okta-auth-js@${published_tarball}; then
+if ! yarn add ../okta-auth-js/${artifact_version}.tgz; then
   echo "yarn-v3 install @okta/okta-auth-js@${published_tarball} failed! Exiting..."
   exit ${FAILED_SETUP}
 fi
