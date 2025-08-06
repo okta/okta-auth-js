2302f3d4925f18bbfb66afda91ffda28bf406906
Jared Perreault <jared.perreault@okta.com>
2025-08-06 13:06:10 -0400

    test

--- scripts/verify-registry-install.sh
+++ scripts/verify-registry-install.sh
@@ -56,7 +56,8 @@ popd
 mkdir yarn-v3-test
 pushd yarn-v3-test
 # use yarn v3
-yarn set version 3.x
+# https://github.com/yarnpkg/yarn/issues/7847#issuecomment-1225000565
+GITHUB_TOKEN="" yarn set version 3.x
 yarn config set caFilePath /etc/pki/tls/certs/ca-bundle.crt
 yarn init -y
 # add empty lock file, so this dir can be a isolated project
